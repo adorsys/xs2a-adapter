5debb9d87d03b2ee1e4c0a73da9d65bfa972801c
Mykola Shumyn <msh@adorsys.com.ua>
2019-04-03 11:23:56 +0300

    Get single payment flow

--- xs2a-gateway-service-impl/src/main/java/de/adorsys/xs2a/gateway/service/impl/PaymentServiceImpl.java
+++ xs2a-gateway-service-impl/src/main/java/de/adorsys/xs2a/gateway/service/impl/PaymentServiceImpl.java
@@ -1,8 +1,6 @@
 package de.adorsys.xs2a.gateway.service.impl;
 
-import de.adorsys.xs2a.gateway.service.PaymentInitiationHeaders;
-import de.adorsys.xs2a.gateway.service.PaymentInitiationRequestResponse;
-import de.adorsys.xs2a.gateway.service.PaymentService;
+import de.adorsys.xs2a.gateway.service.*;
 
 public class PaymentServiceImpl implements PaymentService {
 
@@ -11,5 +9,10 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public PaymentInitiationRequestResponse initiatePayment(String paymentService, String paymentProduct, Object body, PaymentInitiationHeaders headers) {
         return this.paymentService.initiatePayment(paymentService, paymentProduct, body, headers);
+    }
+
+    @Override
+    public PaymentInformationResponse getPaymentInformation(String paymentService, String paymentProduct, String paymentId, PaymentInformationHeaders headers) {
+        return this.paymentService.getPaymentInformation(paymentService, paymentProduct, paymentId, headers);
     }
 }
