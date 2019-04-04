bb6b6dbe76a4f09f270b8a0d93ecf16d847cb01b
Serhii Petrychenko <spe@adorsys.com.ua>
2019-04-04 12:04:38 +0300

    consent service

--- xs2a-gateway-app/src/main/java/de/adorsys/xs2a/gateway/config/RestConfiguration.java
+++ xs2a-gateway-app/src/main/java/de/adorsys/xs2a/gateway/config/RestConfiguration.java
@@ -2,6 +2,8 @@ package de.adorsys.xs2a.gateway.config;
 
 import de.adorsys.xs2a.gateway.mapper.PaymentInitiationScaStatusResponseMapper;
 import de.adorsys.xs2a.gateway.service.PaymentService;
+import de.adorsys.xs2a.gateway.service.consent.ConsentService;
+import de.adorsys.xs2a.gateway.service.impl.ConsentServiceImpl;
 import de.adorsys.xs2a.gateway.service.impl.PaymentServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +18,10 @@ public class RestConfiguration {
     @Bean
     PaymentInitiationScaStatusResponseMapper getPaymentInitiationScaStatusResponseMapper() {
         return new PaymentInitiationScaStatusResponseMapper();
+    }
+
+    @Bean
+    ConsentService consentService() {
+        return new ConsentServiceImpl();
     }
 }
---
+++ xs2a-gateway-service-api/src/main/java/de/adorsys/xs2a/gateway/service/consent/AccountAccess.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018-2019 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.xs2a.gateway.service.consent;
+
+import java.util.List;
+
+/**
+ * Requested access services for a consent.
+ */
+public class AccountAccess {
+    private List<AccountReference> accounts;
+    private List<AccountReference> balances;
+    private List<AccountReference> transactions;
+    private AvailableAccountsEnum availableAccounts;
+    private AllPsd2Enum allPsd2;
+
+    public enum AvailableAccountsEnum {
+        ALLACCOUNTS("allAccounts"),
+
+        ALLACCOUNTSWITHBALANCES("allAccountsWithBalances");
+
+        private String value;
+
+        AvailableAccountsEnum(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+
+        public static AvailableAccountsEnum fromValue(String text) {
+            for (AvailableAccountsEnum b : AvailableAccountsEnum.values()) {
+                if (String.valueOf(b.value).equals(text)) {
+                    return b;
+                }
+            }
+            return null;
+        }
+    }
+
+    public enum AllPsd2Enum {
+        ALLACCOUNTS("allAccounts");
+
+        private String value;
+
+        AllPsd2Enum(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+
+        public static AllPsd2Enum fromValue(String text) {
+            for (AllPsd2Enum b : AllPsd2Enum.values()) {
+                if (String.valueOf(b.value).equals(text)) {
+                    return b;
+                }
+            }
+            return null;
+        }
+    }
+}
+
