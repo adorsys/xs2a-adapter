0318d9da69328324bece9df2a789be153b31cc1c
Serhii Petrychenko <spe@adorsys.com.ua>
2019-04-09 16:27:32 +0300

    add tests

---
+++ deutsche-bank-adapter/src/test/java/de/adorsys/xs2a/gateway/service/impl/DeutscheBankConsentServiceTest.java
@@ -0,0 +1,49 @@
+package de.adorsys.xs2a.gateway.service.impl;
+
+import de.adorsys.xs2a.gateway.service.Headers;
+import de.adorsys.xs2a.gateway.service.consent.ConsentCreationResponse;
+import de.adorsys.xs2a.gateway.service.consent.Consents;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
+public class DeutscheBankConsentServiceTest {
+
+    private static final String CONSENT_URL = "https://simulator-xs2a.db.com/pis/DE/SB-DB/v1/consents";
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void createConsent() {
+        HttpClient httpClient = mock(HttpClient.class);
+        DeutscheBankConsentService service = new DeutscheBankConsentService();
+        service.setHttpClient(httpClient);
+
+        ArgumentCaptor<Map> headersCaptor = ArgumentCaptor.forClass(Map.class);
+
+        when(httpClient.post(
+                eq(CONSENT_URL),
+                anyString(),
+                headersCaptor.capture(),
+                any()
+        )).thenReturn(new ConsentCreationResponse());
+
+        service.createConsent(new Consents(), Headers.builder().build());
+
+        verify(httpClient, times(1)).post(
+                eq(CONSENT_URL),
+                anyString(),
+                headersCaptor.capture(), any());
+
+        Map headers = headersCaptor.getValue();
+
+        assertThat(headers).hasSize(2);
+        assertThat(headers.containsKey("Date")).isTrue();
+        assertThat(headers.get("Content-Type")).isEqualTo("application/json");
+    }
+}
