41fab9b4f566ce47b03694dadf8839ad52d3cd13
Artem Snisarenko <asn@adorsys.com.ua>
2019-04-12 10:04:11 +0300

    Fix dates serialized as json list

--- deutsche-bank-adapter/src/main/java/de/adorsys/xs2a/gateway/service/impl/JsonMapper.java
+++ deutsche-bank-adapter/src/main/java/de/adorsys/xs2a/gateway/service/impl/JsonMapper.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ public class JsonMapper {
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
     public String writeValueAsString(Object value) {
