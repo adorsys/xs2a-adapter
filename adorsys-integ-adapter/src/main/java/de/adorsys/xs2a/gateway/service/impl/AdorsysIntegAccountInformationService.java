8f2ab2dad6efe01d414964824ddeef99bc181527
Serhii Petrychenko <spe@adorsys.com.ua>
2019-04-15 14:22:21 +0300

    add adorsys integ adapter structure

---
+++ adorsys-integ-adapter/src/main/java/de/adorsys/xs2a/gateway/service/impl/AdorsysIntegAccountInformationService.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018-2018 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.xs2a.gateway.service.impl;
+
+import de.adorsys.xs2a.gateway.service.Headers;
+import de.adorsys.xs2a.gateway.service.ais.*;
+
+public class AdorsysIntegAccountInformationService implements AccountInformationService {
+    @Override
+    public ConsentCreationResponse createConsent(Consents body, Headers headers) {
+        return null;
+    }
+
+    @Override
+    public ConsentInformation getConsentInformation(String consentId, Headers headers) {
+        return null;
+    }
+
+    @Override
+    public ConsentStatusResponse getConsentStatus(String consentId, Headers headers) {
+        return null;
+    }
+}
