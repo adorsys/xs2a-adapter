836db9e5b59dfddc40077be654a3f182dc752aaf
Serhii Petrychenko <spe@adorsys.com.ua>
2019-04-22 09:18:47 +0300

    verlag adapter

---
+++ verlag-adapter/src/main/java/de/adorsys/xs2a/gateway/service/provider/VerlagAccountInformationServiceProvider.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018-2018 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.xs2a.gateway.service.provider;
+
+import de.adorsys.xs2a.gateway.service.ais.AccountInformationService;
+import de.adorsys.xs2a.gateway.service.impl.VerlagAccountInformationService;
+
+public class VerlagAccountInformationServiceProvider implements AccountInformationServiceProvider {
+
+    private VerlagAccountInformationService accountInformationService;
+
+    @Override
+    public String getBankCode() {
+        return "25040090";
+    }
+
+    @Override
+    public AccountInformationService getAccountInformationService() {
+        if (accountInformationService == null) {
+            accountInformationService = new VerlagAccountInformationService();
+        }
+        return accountInformationService;
+    }
+}
