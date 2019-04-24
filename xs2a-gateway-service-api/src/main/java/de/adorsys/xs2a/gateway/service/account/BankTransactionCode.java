1563c90f1f8df585ffb322f218525d73db71b591
Artem Snisarenko <asn@adorsys.com.ua>
2019-04-24 10:49:51 +0300

    Fix deserialization for list transactions

--- xs2a-gateway-service-api/src/main/java/de/adorsys/xs2a/gateway/service/account/BankTransactionCode.java
+++ xs2a-gateway-service-api/src/main/java/de/adorsys/xs2a/gateway/service/account/BankTransactionCode.java
@@ -3,6 +3,13 @@ package de.adorsys.xs2a.gateway.service.account;
 public class BankTransactionCode {
     private String code;
 
+    public BankTransactionCode() {
+    }
+
+    public BankTransactionCode(String code) {
+        this.code = code;
+    }
+
     public String getCode() {
         return code;
     }
