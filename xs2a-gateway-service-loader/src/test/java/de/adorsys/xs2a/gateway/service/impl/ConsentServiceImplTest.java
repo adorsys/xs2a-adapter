503fbd3465cd410d1c23c777a627f2c719bab1a1
Serhii Petrychenko <spe@adorsys.com.ua>
2019-04-25 16:37:36 +0300

    fix PR concerns

--- xs2a-gateway-service-loader/src/test/java/de/adorsys/xs2a/gateway/service/impl/ConsentServiceImplTest.java
+++ xs2a-gateway-service-loader/src/test/java/de/adorsys/xs2a/gateway/service/impl/ConsentServiceImplTest.java
@@ -3,8 +3,8 @@ package de.adorsys.xs2a.gateway.service.impl;
 import de.adorsys.xs2a.gateway.service.GeneralResponse;
 import de.adorsys.xs2a.gateway.service.RequestHeaders;
 import de.adorsys.xs2a.gateway.service.ResponseHeaders;
-import de.adorsys.xs2a.gateway.service.ais.ConsentCreationResponse;
 import de.adorsys.xs2a.gateway.service.ais.AccountInformationService;
+import de.adorsys.xs2a.gateway.service.ais.ConsentCreationResponse;
 import de.adorsys.xs2a.gateway.service.ais.Consents;
 import org.junit.Test;
 
@@ -20,19 +20,19 @@ public class ConsentServiceImplTest {
     @Test
     public void createConsent() {
         GeneralResponse<ConsentCreationResponse> response = new GeneralResponse<>(HTTP_CODE_200, new ConsentCreationResponse(), ResponseHeaders.fromMap(Collections.emptyMap()));
-        DeutscheBankAccountInformationService deutscheBankConsentService = mock(DeutscheBankAccountInformationService.class);
+        AccountInformationService accountInformationService = mock(AccountInformationService.class);
         AccountInformationServiceImpl service = new AccountInformationServiceImpl(){
             @Override
             AccountInformationService getConsentService(RequestHeaders headers) {
-                return deutscheBankConsentService;
+                return accountInformationService;
             }
         };
 
-        when(deutscheBankConsentService.createConsent(any(), any())).thenReturn(response);
+        when(accountInformationService.createConsent(any(), any())).thenReturn(response);
 
         GeneralResponse<ConsentCreationResponse> consentResponse = service.createConsent(new Consents(), RequestHeaders.builder().build());
 
-        verify(deutscheBankConsentService, times(1)).createConsent(any(), any());
+        verify(accountInformationService, times(1)).createConsent(any(), any());
 
         assertThat(consentResponse).isEqualTo(response);
     }
