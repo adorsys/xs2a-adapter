2de9b708551ded0f92548d2cdb99eb834d9ce8d8
Mykola Shumyn <msh@adorsys.com.ua>
2019-05-07 15:50:59 +0300

    Updated the design of hashing services

--- xs2a-gateway-service-api-adapter/src/main/java/de/adorsys/xs2a/gateway/signing/service/hashing/Sha256HashingService.java
+++ xs2a-gateway-service-api-adapter/src/main/java/de/adorsys/xs2a/gateway/signing/service/hashing/Sha256HashingService.java
@@ -1,27 +1,12 @@
 package de.adorsys.xs2a.gateway.signing.service.hashing;
 
-import de.adorsys.xs2a.gateway.signing.exception.HttpRequestSigningException;
 import de.adorsys.xs2a.gateway.signing.service.algorithm.HashingAlgorithm;
-
-import java.nio.charset.Charset;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 import static de.adorsys.xs2a.gateway.signing.service.algorithm.HashingAlgorithm.SHA256;
 
-public class Sha256HashingService implements HashingService {
+public class Sha256HashingService extends ShaHashingService {
 
-    @Override
-    public byte[] hash(String data, Charset charset) {
-        try {
-            return MessageDigest.getInstance(getAlgorithm().getAlgorithmName())
-                           .digest(data.getBytes(charset));
-        } catch (NoSuchAlgorithmException e) {
-            throw new HttpRequestSigningException("No such hashing algorithm: " + getAlgorithm().getAlgorithmName());
-        }
-    }
-
-    private HashingAlgorithm getAlgorithm() {
+    protected HashingAlgorithm getAlgorithm() {
         return SHA256;
     }
 }
--- xs2a-gateway-service-api-adapter/src/main/java/de/adorsys/xs2a/gateway/signing/service/hashing/Sha512HashingService.java
+++ xs2a-gateway-service-api-adapter/src/main/java/de/adorsys/xs2a/gateway/signing/service/hashing/Sha512HashingService.java
@@ -1,27 +1,12 @@
 package de.adorsys.xs2a.gateway.signing.service.hashing;
 
-import de.adorsys.xs2a.gateway.signing.exception.HttpRequestSigningException;
 import de.adorsys.xs2a.gateway.signing.service.algorithm.HashingAlgorithm;
-
-import java.nio.charset.Charset;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 import static de.adorsys.xs2a.gateway.signing.service.algorithm.HashingAlgorithm.SHA512;
 
-public class Sha512HashingService implements HashingService {
+public class Sha512HashingService extends ShaHashingService {
 
-    @Override
-    public byte[] hash(String data, Charset charset) {
-        try {
-            return MessageDigest.getInstance(getAlgorithm().getAlgorithmName())
-                           .digest(data.getBytes(charset));
-        } catch (NoSuchAlgorithmException e) {
-            throw new HttpRequestSigningException("No such hashing algorithm: " + getAlgorithm().getAlgorithmName());
-        }
-    }
-
-    private HashingAlgorithm getAlgorithm() {
+    protected HashingAlgorithm getAlgorithm() {
         return SHA512;
     }
 }
