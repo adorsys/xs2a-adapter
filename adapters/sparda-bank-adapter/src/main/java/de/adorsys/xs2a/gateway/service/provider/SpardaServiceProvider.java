51516ab661b19556d5c4690fc845907bbe845e9e
Artem Snisarenko <asn@adorsys.com.ua>
2019-05-09 10:40:08 +0300

    Add PIS for sparda-bank

--- adapters/sparda-bank-adapter/src/main/java/de/adorsys/xs2a/gateway/service/provider/SpardaServiceProvider.java
+++ adapters/sparda-bank-adapter/src/main/java/de/adorsys/xs2a/gateway/service/provider/SpardaServiceProvider.java
@@ -17,17 +17,20 @@
 package de.adorsys.xs2a.gateway.service.provider;
 
 import de.adorsys.xs2a.gateway.adapter.BaseAccountInformationService;
+import de.adorsys.xs2a.gateway.adapter.BasePaymentInitiationService;
+import de.adorsys.xs2a.gateway.service.PaymentInitiationService;
 import de.adorsys.xs2a.gateway.service.ais.AccountInformationService;
 
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-public class SpardaAccountInformationServiceProvider implements AccountInformationServiceProvider {
+public class SpardaServiceProvider implements AccountInformationServiceProvider, PaymentInitiationServiceProvider{
 
     private static final String BASE_URI = "https://api.sparda.de.schulung.sparda.de/v1";
     private Set<String> bankCodes = Collections.unmodifiableSet(new HashSet<>(Collections.singletonList("sparda-bank")));
     private AccountInformationService accountInformationService;
+    private PaymentInitiationService paymentInitiationService;
 
     @Override
     public Set<String> getBankCodes() {
@@ -40,5 +43,13 @@ public class SpardaAccountInformationServiceProvider implements AccountInformati
             accountInformationService = new BaseAccountInformationService(BASE_URI);
         }
         return accountInformationService;
+    }
+
+    @Override
+    public PaymentInitiationService getPaymentInitiationService() {
+        if (paymentInitiationService == null) {
+            paymentInitiationService = new BasePaymentInitiationService(BASE_URI);
+        }
+        return paymentInitiationService;
     }
 }
