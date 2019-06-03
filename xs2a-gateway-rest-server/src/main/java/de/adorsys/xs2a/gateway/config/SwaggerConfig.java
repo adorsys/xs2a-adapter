c89a713a421d78c93b403fbba6d0033522b18620
Artem Snisarenko <asn@adorsys.com.ua>
2019-06-03 10:34:04 +0300

    Use json instead of annotation for swagger doc

--- xs2a-gateway-rest-server/src/main/java/de/adorsys/xs2a/gateway/config/SwaggerConfig.java
+++ xs2a-gateway-rest-server/src/main/java/de/adorsys/xs2a/gateway/config/SwaggerConfig.java
@@ -1,38 +1,25 @@
 package de.adorsys.xs2a.gateway.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    @Value("${info.app.version}")
-    private String version;
-
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                       .apiInfo(apiInfo())
-                       .select()
-                       .apis(RequestHandlerSelectors.basePackage("de.adorsys.xs2a.gateway.controller"))
-                       .paths(PathSelectors.any())
-                       .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                       .title("xs2a Gateway API")
-                       .description("")
-                       .version(version)
-                       .build();
+    @Primary
+    public SwaggerResourcesProvider swaggerResourcesProvider() {
+        return () -> {
+            SwaggerResource swaggerResource = new SwaggerResource();
+            swaggerResource.setLocation("/openapi.json");
+            return Collections.singletonList(swaggerResource);
+        };
     }
 }
