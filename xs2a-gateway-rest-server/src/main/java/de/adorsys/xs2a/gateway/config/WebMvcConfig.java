48c3afda321d211189fa1b19856ea49792c15306
Artem Snisarenko <asn@adorsys.com.ua>
2019-06-03 11:12:12 +0300

    Fix booking status convert error

--- xs2a-gateway-rest-server/src/main/java/de/adorsys/xs2a/gateway/config/WebMvcConfig.java
+++ xs2a-gateway-rest-server/src/main/java/de/adorsys/xs2a/gateway/config/WebMvcConfig.java
@@ -1,17 +1,27 @@
 package de.adorsys.xs2a.gateway.config;
 
+import de.adorsys.xs2a.gateway.model.BookingStatusTO;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class DateTimeFormatConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
         DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
         registrar.setUseIsoFormat(true);
         registrar.registerFormatters(registry);
+
+        registry.addConverter(new Converter<String, BookingStatusTO>() {
+
+            @Override
+            public BookingStatusTO convert(String source) {
+                return BookingStatusTO.fromValue(source);
+            }
+        });
     }
 }
