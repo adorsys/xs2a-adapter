edb599f3d4ade5277707b0486e1f4c2af05f932d
Artem Snisarenko <asn@adorsys.com.ua>
2019-07-04 14:46:10 +0300

    Revert removal of GeneralInformationService

--- xs2a-adapter-rest-server/src/main/java/de/adorsys/xs2a/adapter/mapper/BankMapper.java
+++ xs2a-adapter-rest-server/src/main/java/de/adorsys/xs2a/adapter/mapper/BankMapper.java
@@ -1,17 +1,13 @@
 package de.adorsys.xs2a.adapter.mapper;
 
 import de.adorsys.xs2a.adapter.model.BankTO;
-import de.adorsys.xs2a.adapter.service.impl.AspspAdapterConfigRecord;
+import de.adorsys.xs2a.adapter.service.Bank;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper
 public interface BankMapper {
 
-    @Mapping(target = "name", source = "aspspName")
-    BankTO toBankTO(AspspAdapterConfigRecord record);
-
-    List<BankTO> toBankTOList(List<AspspAdapterConfigRecord> configRecords);
+    List<BankTO> toBankTOList(List<Bank> configRecords);
 }
