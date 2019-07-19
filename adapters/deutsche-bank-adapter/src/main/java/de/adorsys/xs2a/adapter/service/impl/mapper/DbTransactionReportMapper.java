e50fdd10780ba644f3998d982b6b1772444f8734
Serhii Petrychenko <spe@adorsys.com.ua>
2019-07-19 16:37:18 +0300

    renamed map

--- adapters/deutsche-bank-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/mapper/DbTransactionReportMapper.java
+++ adapters/deutsche-bank-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/mapper/DbTransactionReportMapper.java
@@ -31,18 +31,18 @@ public interface DbTransactionReportMapper {
 
     default TransactionsReport toTransactionReport(DbTransactionReport report) {
         TransactionsReport transactionsReport = new TransactionsReport();
-        Map<String, Link> map = new HashMap<>();
+        Map<String, Link> linkMap = new HashMap<>();
         DbLinks links = report.getLinks();
         Link account = links.getAccount();
         Link balance = links.getBalance();
         List<Link> downloads = links.getDownloads();
-        map.put("account", account);
-        map.put("balance", balance);
+        linkMap.put("account", account);
+        linkMap.put("balance", balance);
         for (int i = 0; i < downloads.size(); i++) {
             Link download = downloads.get(i);
-            map.put("download"+i, download);
+            linkMap.put("download"+i, download);
         }
-        transactionsReport.setLinks(map);
+        transactionsReport.setLinks(linkMap);
         return transactionsReport;
     }
 }
