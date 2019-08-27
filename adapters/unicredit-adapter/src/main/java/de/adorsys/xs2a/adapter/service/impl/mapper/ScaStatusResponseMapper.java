fec044bec544d4d0a2384b5b50e8aa9ac0c6fd63
Mykola Shumyn <msh@adorsys.com.ua>
2019-08-27 11:41:57 +0300

    Refactored SCA status mapping by separating statuses mapping into the concrete mapper

--- adapters/unicredit-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/mapper/ScaStatusResponseMapper.java
+++ adapters/unicredit-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/mapper/ScaStatusResponseMapper.java
@@ -1,27 +1,32 @@
 package de.adorsys.xs2a.adapter.service.impl.mapper;
 
 import de.adorsys.xs2a.adapter.service.ScaStatus;
-import de.adorsys.xs2a.adapter.service.TransactionStatus;
 import de.adorsys.xs2a.adapter.service.ais.ConsentStatus;
+import de.adorsys.xs2a.adapter.service.ais.ConsentStatusResponse;
 import de.adorsys.xs2a.adapter.service.impl.model.UnicreditAccountScaStatusResponse;
 import de.adorsys.xs2a.adapter.service.impl.model.UnicreditPaymentScaStatusResponse;
 import de.adorsys.xs2a.adapter.service.model.ScaStatusResponse;
 
+import java.util.Optional;
+
 public class ScaStatusResponseMapper {
+    private final ScaStatusMapper scaStatusMapper = new ScaStatusMapper();
 
     public ScaStatusResponse toScaStatusResponse(UnicreditAccountScaStatusResponse unicreditAccountScaStatusResponse) {
-        ConsentStatus consentStatus = unicreditAccountScaStatusResponse.getConsentStatus();
+        ScaStatus scaStatus = scaStatusMapper.toScaStatus(unicreditAccountScaStatusResponse.getConsentStatus());
 
-        ScaStatus scaStatus;
+        ScaStatusResponse scaStatusResponse = new ScaStatusResponse();
+        scaStatusResponse.setScaStatus(scaStatus);
 
-        if (consentStatus == ConsentStatus.VALID) {
-            scaStatus = ScaStatus.FINALISED;
-        } else if (consentStatus == ConsentStatus.REJECTED) {
-            scaStatus = ScaStatus.FAILED;
-        } else {
-            // TODO come up with better solution
-            throw new RuntimeException();
-        }
+        return scaStatusResponse;
+    }
+
+    public ScaStatusResponse toScaStatusResponse(ConsentStatusResponse consentStatusResponse) {
+        ConsentStatus consentStatus = Optional.ofNullable(consentStatusResponse)
+                                          .map(ConsentStatusResponse::getConsentStatus)
+                                          .orElse(null);
+
+        ScaStatus scaStatus = scaStatusMapper.toScaStatus(consentStatus);
 
         ScaStatusResponse scaStatusResponse = new ScaStatusResponse();
         scaStatusResponse.setScaStatus(scaStatus);
@@ -30,18 +35,7 @@ public class ScaStatusResponseMapper {
     }
 
     public ScaStatusResponse toScaStatusResponse(UnicreditPaymentScaStatusResponse unicreditPaymentScaStatusResponse) {
-        TransactionStatus transactionStatus = unicreditPaymentScaStatusResponse.getTransactionStatus();
-
-        ScaStatus scaStatus;
-
-        if (transactionStatus == TransactionStatus.ACCP) {
-            scaStatus = ScaStatus.FINALISED;
-        } else if (transactionStatus == TransactionStatus.RJCT) {
-            scaStatus = ScaStatus.FAILED;
-        } else {
-            // TODO come up with better solution
-            throw new RuntimeException();
-        }
+        ScaStatus scaStatus = scaStatusMapper.toScaStatus(unicreditPaymentScaStatusResponse.getTransactionStatus());
 
         ScaStatusResponse scaStatusResponse = new ScaStatusResponse();
         scaStatusResponse.setScaStatus(scaStatus);
