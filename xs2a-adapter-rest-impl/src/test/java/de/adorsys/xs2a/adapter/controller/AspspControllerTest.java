65aded717d94813545e0ebc421cef8d087499eeb
Oleksandr Berezkin <oleksandrberezkin@Oleksandrs-MacBook-Pro.local>
2019-09-06 16:29:17 +0300

    remove unused libs

--- xs2a-adapter-rest-impl/src/test/java/de/adorsys/xs2a/adapter/controller/AspspControllerTest.java
+++ xs2a-adapter-rest-impl/src/test/java/de/adorsys/xs2a/adapter/controller/AspspControllerTest.java
@@ -17,7 +17,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -32,7 +31,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
--- xs2a-adapter-service-api/src/main/java/de/adorsys/xs2a/adapter/service/AspspCsvService.java
+++ xs2a-adapter-service-api/src/main/java/de/adorsys/xs2a/adapter/service/AspspCsvService.java
@@ -1,7 +1,5 @@
 package de.adorsys.xs2a.adapter.service;
 
-import java.util.List;
-
 public interface AspspCsvService {
 
     byte[] exportCsv();
