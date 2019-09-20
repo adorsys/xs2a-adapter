cd95791ced62724f76dcbbb701064a47a72bc52c
Oleksandr Berezkin <oleksandrberezkin@Oleksandrs-MacBook-Pro.local>
2019-09-20 17:11:41 +0300

    bug fix

--- xs2a-adapter-rest-impl/src/main/resources/static/main.js
+++ xs2a-adapter-rest-impl/src/main/resources/static/main.js
@@ -87,29 +87,29 @@ function addRow() {
         if (e.className) {
             if (e.className.indexOf("edit") > -1) {
                 let helper = e.parentNode.childNodes[7];
-    
+
                 e.addEventListener("click", () => { editButton(e) })
                 e.setAttribute("id", editId + COUNTER);
-    
+
                 helper.setAttribute("data-mdl-for", editId + COUNTER);
                 editButton(e);
             }
-    
+
             if (e.className.indexOf("update") > -1) {
                 let helper = e.parentNode.childNodes[9];
-    
+
                 e.addEventListener("click", () => { greenButton(e) })
                 e.setAttribute("id", updateId + COUNTER);
-    
+
                 helper.setAttribute("data-mdl-for", updateId + COUNTER);
             }
-    
+
             if (e.className.indexOf("delete") > -1) {
                 let helper = e.parentNode.childNodes[11];
-    
+
                 e.addEventListener("click", () => { redButton(e) })
                 e.setAttribute("id", deleteId + COUNTER);
-    
+
                 helper.setAttribute("data-mdl-for", deleteId + COUNTER);
             }
         }
@@ -217,6 +217,8 @@ function updateButton(e) {
 }
 
 function saveButton(e) {
+    let row = e.parentElement.parentElement;
+
     fetch("/v1/aspsps/", {
         method: 'POST',
         headers: {
@@ -251,7 +253,7 @@ function buildRow(data) {
 
     let clone = HIDDEN_ROW.cloneNode(true);
     clone.removeAttribute("class");
-    
+
     clone.cells[0].textContent = data.id;
     clone.cells[1].textContent = data.name;
     clone.cells[2].textContent = data.bic;
@@ -263,28 +265,28 @@ function buildRow(data) {
         if (e.className) {
             if (e.className.indexOf("edit") > -1) {
                 let helper = e.parentNode.childNodes[7];
-    
+
                 e.addEventListener("click", () => { editButton(e) })
                 e.setAttribute("id", editId + COUNTER);
-    
+
                 helper.setAttribute("data-mdl-for", editId + COUNTER);
             }
-    
+
             if (e.className.indexOf("update") > -1) {
                 let helper = e.parentNode.childNodes[9];
-    
+
                 e.addEventListener("click", () => { greenButton(e) })
                 e.setAttribute("id", updateId + COUNTER);
-    
+
                 helper.setAttribute("data-mdl-for", updateId + COUNTER);
             }
-    
+
             if (e.className.indexOf("delete") > -1) {
                 let helper = e.parentNode.childNodes[11];
-    
+
                 e.addEventListener("click", () => { redButton(e) })
                 e.setAttribute("id", deleteId + COUNTER);
-    
+
                 helper.setAttribute("data-mdl-for", deleteId + COUNTER);
             }
         }
