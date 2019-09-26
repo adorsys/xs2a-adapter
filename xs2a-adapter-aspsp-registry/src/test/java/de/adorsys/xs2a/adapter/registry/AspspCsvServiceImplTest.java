c6362365eeec8f9334f605b5eeebf22cc0f466c9
Mykola Shumyn <msh@adorsys.com.ua>
2019-09-26 19:03:58 +0300

    Updated tests

--- xs2a-adapter-aspsp-registry/src/test/java/de/adorsys/xs2a/adapter/registry/AspspCsvServiceImplTest.java
+++ xs2a-adapter-aspsp-registry/src/test/java/de/adorsys/xs2a/adapter/registry/AspspCsvServiceImplTest.java
@@ -2,6 +2,7 @@ package de.adorsys.xs2a.adapter.registry;
 
 import de.adorsys.xs2a.adapter.service.AspspRepository;
 import de.adorsys.xs2a.adapter.service.model.Aspsp;
+import de.adorsys.xs2a.adapter.service.model.AspspScaApproach;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -9,11 +10,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.*;
-
-import static org.mockito.Mockito.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 public class AspspCsvServiceImplTest {
 
@@ -23,8 +26,10 @@ public class AspspCsvServiceImplTest {
     private static final String URL = "https://xs2a-sndbx.consorsbank.de";
     private static final String ADAPTER_ID = "consors-bank-adapter";
     private static final String BANK_CODE = "76030080";
+    private static final String IDP_URL = "https://example.com";
+    private static final List<AspspScaApproach> SCA_APPROACHES = Arrays.asList(AspspScaApproach.EMBEDDED, AspspScaApproach.REDIRECT);
 
-    private static final byte[] STORED_BYTES_TEMPLATE = "81cecc67-6d1b-4169-b67c-2de52b99a0cc,\"BNP Paribas Germany, Consorsbank\",CSDBDE71XXX,https://xs2a-sndbx.consorsbank.de,consors-bank-adapter,76030080\n".getBytes();
+    private static final byte[] STORED_BYTES_TEMPLATE = "81cecc67-6d1b-4169-b67c-2de52b99a0cc,\"BNP Paribas Germany, Consorsbank\",CSDBDE71XXX,https://xs2a-sndbx.consorsbank.de,consors-bank-adapter,76030080,https://example.com,EMBEDDED;REDIRECT\n".getBytes();
 
     @Mock
     private AspspRepository aspspRepository;
@@ -47,6 +52,8 @@ public class AspspCsvServiceImplTest {
         aspsp.setUrl(URL);
         aspsp.setAdapterId(ADAPTER_ID);
         aspsp.setBankCode(BANK_CODE);
+        aspsp.setIdpUrl(IDP_URL);
+        aspsp.setScaApproaches(SCA_APPROACHES);
 
         when(aspspRepository.findAll()).thenReturn(Collections.singletonList(aspsp));
 
@@ -68,6 +75,8 @@ public class AspspCsvServiceImplTest {
         aspsp.setUrl(URL);
         aspsp.setAdapterId(ADAPTER_ID);
         aspsp.setBankCode(BANK_CODE);
+        aspsp.setIdpUrl(IDP_URL);
+        aspsp.setScaApproaches(SCA_APPROACHES);
 
         repository.add(aspsp);
 
