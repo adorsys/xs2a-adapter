1e325daffa50111d6cd46179ca9308e225e5c6cf
Željko Tomić <zeljko.tomic@nth.ch>
2019-10-02 11:00:55 +0200

    xs2a-adapter-rest-impl: Remove ObjectMapper bean, it is autoconfigured with Spring Boot and change default jackson configuration in xs2a-adapter-app with same settings

--- xs2a-adapter-rest-impl/src/main/java/de/adorsys/xs2a/adapter/config/WebMvcConfig.java
+++ xs2a-adapter-rest-impl/src/main/java/de/adorsys/xs2a/adapter/config/WebMvcConfig.java
@@ -1,14 +1,8 @@
 package de.adorsys.xs2a.adapter.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import de.adorsys.xs2a.adapter.model.BookingStatusTO;
 import de.adorsys.xs2a.adapter.model.PaymentProductTO;
 import de.adorsys.xs2a.adapter.model.PaymentServiceTO;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
@@ -82,16 +76,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     }
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-        return objectMapper;
-    }
 }
