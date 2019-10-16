0efb7dcf754925cd4e0c387f475eaf6abbd3e1e6
Mykola Shumyn <msh@adorsys.com.ua>
2019-10-16 12:49:13 +0300

    Fixed checkstyle javadocs violations

--- xs2a-adapter-service-api/src/main/java/de/adorsys/xs2a/adapter/service/AspspService.java
+++ xs2a-adapter-service-api/src/main/java/de/adorsys/xs2a/adapter/service/AspspService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface AspspService {
 
     /**
-     * Saves Aspsp instance into the Lucene storage
+     * Saves Aspsp instance into the Lucene storage.
      *
      * @param aspsp instance of {@link Aspsp}
      * @return saved instance of {@link Aspsp}
@@ -15,7 +15,7 @@ public interface AspspService {
     Aspsp create(Aspsp aspsp);
 
     /**
-     * Updates Aspsp record in the Lucene storage
+     * Updates Aspsp record in the Lucene storage.
      *
      * @param aspsp instance of {@link Aspsp}
      * @return updated instance of {@link Aspsp}
@@ -23,14 +23,14 @@ public interface AspspService {
     Aspsp update(Aspsp aspsp);
 
     /**
-     * Deletes Aspsp record by its identifier from Lucene storage
+     * Deletes Aspsp record by its identifier from Lucene storage.
      *
      * @param aspspId Aspsp identifier
      */
     void deleteById(String aspspId);
 
     /**
-     * Reads all Aspsp records from Lucene storage
+     * Reads all Aspsp records from Lucene storage.
      *
      * @return list of {@link Aspsp} instances
      */
@@ -38,7 +38,7 @@ public interface AspspService {
 
     /**
      * Replaces all the Aspsp records in Lucene storage
-     * with the list of {@link Aspsp} instances
+     * with the list of {@link Aspsp} instances.
      *
      * @param aspsps list of {@link Aspsp} instances
      */
