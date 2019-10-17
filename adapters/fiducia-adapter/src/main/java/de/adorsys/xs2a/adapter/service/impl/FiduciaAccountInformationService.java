bde44b6cee3296723829ce806c0d97b7128f04ad
n0rdy <msh@adorsys.com.ua>
2019-10-17 08:41:23 +0300

    Fiducia supported booking statuses (#251)
    * Updated Fiducia get transactions flow to make it respond with 400 on unsupported booking status in request
    
    * Provided tests for FiduciaAccountInformationService
    
    * Added release notes

--- adapters/fiducia-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/FiduciaAccountInformationService.java
+++ adapters/fiducia-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/FiduciaAccountInformationService.java
@@ -19,14 +19,28 @@ package de.adorsys.xs2a.adapter.service.impl;
 import de.adorsys.xs2a.adapter.adapter.BaseAccountInformationService;
 import de.adorsys.xs2a.adapter.http.HttpClient;
 import de.adorsys.xs2a.adapter.http.Request;
+import de.adorsys.xs2a.adapter.service.RequestHeaders;
+import de.adorsys.xs2a.adapter.service.RequestParams;
+import de.adorsys.xs2a.adapter.service.Response;
+import de.adorsys.xs2a.adapter.service.exception.BadRequestException;
+import de.adorsys.xs2a.adapter.service.model.TransactionsReport;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import static org.apache.http.protocol.HTTP.DATE_HEADER;
 
 public class FiduciaAccountInformationService extends BaseAccountInformationService {
+    private static final Set<String> SUPPORTED_BOOKING_STATUSES = new HashSet<>(Collections.singletonList("booked"));
+    private static final String BOOKING_STATUS_ERROR_MESSAGE = String.format(
+        "ASPSP supports only the following booking statuses: %s. " +
+            "The booking status from the request has to be changed to the supported ones.",
+        SUPPORTED_BOOKING_STATUSES
+    );
 
     public FiduciaAccountInformationService(String baseUri,
                                             HttpClient httpClient,
@@ -56,5 +70,28 @@ public class FiduciaAccountInformationService extends BaseAccountInformationServ
     protected Map<String, String> populateDeleteHeaders(Map<String, String> headers) {
         headers.put(DATE_HEADER, DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now()));
         return headers;
+    }
+
+    @Override
+    public Response<TransactionsReport> getTransactionList(String accountId, RequestHeaders requestHeaders, RequestParams requestParams) {
+        if (notSupportedBookingStatus(requestParams)) {
+            throw new BadRequestException(BOOKING_STATUS_ERROR_MESSAGE);
+        }
+
+        return super.getTransactionList(accountId, requestHeaders, requestParams);
+    }
+
+    @Override
+    public Response<String> getTransactionListAsString(String accountId, RequestHeaders requestHeaders, RequestParams requestParams) {
+        if (notSupportedBookingStatus(requestParams)) {
+            throw new BadRequestException(BOOKING_STATUS_ERROR_MESSAGE);
+        }
+
+        return super.getTransactionListAsString(accountId, requestHeaders, requestParams);
+    }
+
+    private boolean notSupportedBookingStatus(RequestParams requestParams) {
+        String bookingStatus = requestParams.toMap().get(RequestParams.BOOKING_STATUS);
+        return bookingStatus != null && !SUPPORTED_BOOKING_STATUSES.contains(bookingStatus);
     }
 }
