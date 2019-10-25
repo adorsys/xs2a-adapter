3980478e853f4e7c5153c6134b024c6a03e53c94
Serhii Petrychenko <spe@adorsys.com.ua>
2019-10-25 10:50:13 +0300

    add support of multiple bank codes

--- adapters/adorsys-integ-adapter/src/main/java/de/adorsys/xs2a/adapter/adorsys/service/provider/OauthHeaderInterceptor.java
+++ adapters/adorsys-integ-adapter/src/main/java/de/adorsys/xs2a/adapter/adorsys/service/provider/OauthHeaderInterceptor.java
@@ -20,25 +20,32 @@ import de.adorsys.xs2a.adapter.http.Request;
 import de.adorsys.xs2a.adapter.service.RequestHeaders;
 import de.adorsys.xs2a.adapter.service.config.AdapterConfig;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class OauthHeaderInterceptor implements Request.Builder.Interceptor {
 
+    private static final String BANK_CODE_FOR_OAUTH = "adorsys.oauth_approach.bank_code";
     static final String OAUTH_HEADER_NAME = "adorsys.oauth_approach.header_name";
     static final String OAUTH_HEADER_VALUE = "adorsys.oauth_approach.header_value";
-    static final String BANK_CODE_FOR_OAUTH = "adorsys.oauth_approach.bank_code";
 
     @Override
     public Request.Builder apply(Request.Builder builder) {
         String oauthBankCode = AdapterConfig.readProperty(BANK_CODE_FOR_OAUTH, "");
+        List<String> bankCodes = Arrays.asList(oauthBankCode.split(","));
         String requestBankCode = builder.headers().get(RequestHeaders.X_GTW_BANK_CODE);
 
-        if (oauthBankCode.isEmpty() || !oauthBankCode.equals(requestBankCode)) {
+        if (oauthBankCode.isEmpty() || !bankCodes.contains(requestBankCode)) {
             return builder;
         }
 
+        int idx = bankCodes.indexOf(requestBankCode);
         String oauthHeader = AdapterConfig.readProperty(OAUTH_HEADER_NAME, "");
+        String headerValue = AdapterConfig.readProperty(OAUTH_HEADER_VALUE, "true");
+        List<String> headerValues = Arrays.asList(headerValue.split(","));
 
-        if (!oauthHeader.isEmpty()) {
-            builder.header(oauthHeader, AdapterConfig.readProperty(OAUTH_HEADER_VALUE, "true"));
+        if (!oauthHeader.isEmpty() && headerValues.size() > idx) {
+            builder.header(oauthHeader, headerValues.get(idx));
         }
 
         return builder;
--- adapters/adorsys-integ-adapter/src/test/java/de/adorsys/xs2a/adapter/adorsys/service/provider/OauthHeaderInterceptorTest.java
+++ adapters/adorsys-integ-adapter/src/test/java/de/adorsys/xs2a/adapter/adorsys/service/provider/OauthHeaderInterceptorTest.java
@@ -18,10 +18,15 @@ public class OauthHeaderInterceptorTest {
         String value = AdapterConfig.readProperty(OAUTH_HEADER_VALUE);
 
         RequestBuilderImpl builder = new RequestBuilderImpl(null, null, null);
-        builder.header(RequestHeaders.X_GTW_BANK_CODE, "9001001");
         OauthHeaderInterceptor interceptor = new OauthHeaderInterceptor();
 
-        assertThat(interceptor.apply(builder).headers().get(header), is(value));
+        // pre-step case
+        builder.header(RequestHeaders.X_GTW_BANK_CODE, "90001001");
+        assertThat(interceptor.apply(builder).headers().get(header), is("pre-step"));
+
+        // integrated case
+        builder.header(RequestHeaders.X_GTW_BANK_CODE, "90001002");
+        assertThat(interceptor.apply(builder).headers().get(header), is("integrated"));
     }
 
     @Test
