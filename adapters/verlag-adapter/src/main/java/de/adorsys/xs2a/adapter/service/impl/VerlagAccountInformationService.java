0d69dae2fda22d8d27158e8140af69eef1ce36b6
n0rdy <msh@adorsys.com.ua>
2019-11-06 14:22:08 +0200

    Provided mapping of Accept header for Verlag adapter (#269)
    * Provided mapping of Accept header for Verlag adapter
    
    * Updated mapping of Accept header for Verlag adapter to make it handle accept header as a list of values
    
    * Updated mapping of Accept header for Verlag adapter to make it handle accept header as a list of values
    
    * Added release notes

--- adapters/verlag-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/VerlagAccountInformationService.java
+++ adapters/verlag-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/VerlagAccountInformationService.java
@@ -18,17 +18,66 @@ package de.adorsys.xs2a.adapter.service.impl;
 
 import de.adorsys.xs2a.adapter.adapter.BaseAccountInformationService;
 import de.adorsys.xs2a.adapter.http.HttpClient;
+import de.adorsys.xs2a.adapter.service.RequestHeaders;
+import de.adorsys.xs2a.adapter.service.RequestParams;
+import de.adorsys.xs2a.adapter.service.Response;
 
 import java.util.AbstractMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class VerlagAccountInformationService extends BaseAccountInformationService {
+    private static final String ACCEPT_ALL = "*/*";
+    private static final String ACCEPT_XML = "application/xml";
 
     private AbstractMap.SimpleImmutableEntry<String, String> apiKey;
 
     public VerlagAccountInformationService(String baseUri, AbstractMap.SimpleImmutableEntry<String, String> apiKey, HttpClient httpClient) {
         super(baseUri, httpClient);
         this.apiKey = apiKey;
+    }
+
+    @Override
+    public Response<String> getTransactionListAsString(String accountId, RequestHeaders requestHeaders, RequestParams requestParams) {
+        return super.getTransactionListAsString(accountId, modifyAcceptHeader(requestHeaders), requestParams);
+    }
+
+    // Needed to deal with the behaviour of ASPSP, that responds with 406
+    // on non existing, empty, equal to */* or list formatted Accept header
+    private RequestHeaders modifyAcceptHeader(RequestHeaders requestHeaders) {
+        Optional<String> acceptHeaderOptional = requestHeaders.get(RequestHeaders.ACCEPT);
+
+        if (!acceptHeaderOptional.isPresent()
+                || acceptHeaderOptional.get().isEmpty()
+                || acceptHeaderOptional.get().equals(ACCEPT_ALL)) {
+            requestHeaders = modifyAcceptHeader(requestHeaders, ACCEPT_XML);
+        } else if (acceptHeaderIsAListOfValues(acceptHeaderOptional.get())) {
+            String[] acceptHeaderValues = acceptHeaderOptional.get().split(",");
+
+            if (containsXml(acceptHeaderValues)) {
+                requestHeaders = modifyAcceptHeader(requestHeaders, ACCEPT_XML);
+            } else {
+                requestHeaders = modifyAcceptHeader(requestHeaders, acceptHeaderValues[0]);
+            }
+        }
+
+        return requestHeaders;
+    }
+
+    private boolean acceptHeaderIsAListOfValues(String acceptHeader) {
+        return acceptHeader.contains(",");
+    }
+
+    private boolean containsXml(String[] acceptHeaderValues) {
+        return Stream.of(acceptHeaderValues)
+                   .anyMatch(accept -> accept.contains(ACCEPT_XML));
+    }
+
+    private RequestHeaders modifyAcceptHeader(RequestHeaders requestHeaders, String acceptHeader) {
+        Map<String, String> headersMap = requestHeaders.toMap();
+        headersMap.put(RequestHeaders.ACCEPT, acceptHeader);
+        return RequestHeaders.fromMap(headersMap);
     }
 
     @Override
