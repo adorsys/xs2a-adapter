65dc116e752b2726370d0ad328dad091c24c2753
berezkin88 <berezkin05@gmail.com>
2019-12-02 19:39:53 +0200

    erase redundant headers for unicredit

--- adapters/unicredit-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/UnicreditAccountInformationService.java
+++ adapters/unicredit-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/UnicreditAccountInformationService.java
@@ -19,39 +19,9 @@ import java.util.Map;
 import static de.adorsys.xs2a.adapter.http.ResponseHandlers.jsonResponseHandler;
 
 public class UnicreditAccountInformationService extends BaseAccountInformationService {
-//    private static final String AUTHENTICATION_CURRENT_NUMBER_QUERY_PARAM = "authenticationCurrentNumber";
-    private static final String DEFAULT_PSU_IP_ADDRESS = "0.0.0.0";
-
-//    private final ScaStatusResponseMapper scaStatusResponseMapper = new ScaStatusResponseMapper();
 
     public UnicreditAccountInformationService(String baseUri, HttpClient httpClient) {
         super(baseUri, httpClient);
-    }
-
-//    @Override
-//    public Response<ScaStatusResponse> updateConsentsPsuData(String consentId, String authorisationId, RequestHeaders requestHeaders, TransactionAuthorisation transactionAuthorisation) {
-//        return updateConsentsPsuData(consentId, authorisationId, requestHeaders, transactionAuthorisation, UnicreditAccountScaStatusResponse.class, scaStatusResponseMapper::toScaStatusResponse);
-//    }
-//
-//    @Override
-//    public Response<ScaStatusResponse> getConsentScaStatus(String consentId, String authorisationId, RequestHeaders requestHeaders) {
-//        Response<ConsentStatusResponse> response = this.getConsentStatus(consentId, requestHeaders);
-//        return new Response<>(response.getStatusCode(), scaStatusResponseMapper.toScaStatusResponse(response.getBody()), response.getHeaders());
-//    }
-//
-//    @Override
-//    protected String getUpdateConsentPsuDataUri(String consentId, String authorisationId) {
-//        String uri = StringUri.fromElements(getConsentBaseUri(), consentId);
-//        return StringUri.withQuery(uri, AUTHENTICATION_CURRENT_NUMBER_QUERY_PARAM, authorisationId);
-//    }
-
-    @Override
-    protected Map<String, String> populateGetHeaders(Map<String, String> map) {
-        Map<String, String> headers = super.populateGetHeaders(map);
-        headers.put(ACCEPT_HEADER, ContentType.APPLICATION_JSON);
-        headers.putIfAbsent(RequestHeaders.PSU_IP_ADDRESS, DEFAULT_PSU_IP_ADDRESS);
-
-        return headers;
     }
 
     @Override
