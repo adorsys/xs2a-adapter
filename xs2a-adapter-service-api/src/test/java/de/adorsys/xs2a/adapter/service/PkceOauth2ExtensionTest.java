ae2f91ab2b8212f5fb9752d98d49e1ae3c6f87da
Artem Snisarenko <artemsnisarenko@users.noreply.github.com>
2020-01-13 16:46:31 +0200

    Fix invalid code verifier (#317)

---
+++ xs2a-adapter-service-api/src/test/java/de/adorsys/xs2a/adapter/service/PkceOauth2ExtensionTest.java
@@ -0,0 +1,30 @@
+package de.adorsys.xs2a.adapter.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PkceOauth2ExtensionTest {
+
+    private final PkceExample pkceExample = new PkceExample();
+
+    // https://tools.ietf.org/html/rfc7636#appendix-B
+    static class PkceExample implements PkceOauth2Extension {
+        @Override
+        public byte[] octetSequence() {
+            return new byte[]{116, 24, (byte) 223, (byte) 180, (byte) 151, (byte) 153, (byte) 224, 37, 79, (byte) 250, 96,
+                125, (byte) 216, (byte) 173, (byte) 187, (byte) 186, 22, (byte) 212, 37, 77, 105, (byte) 214, (byte) 191,
+                (byte) 240, 91, 88, 5, 88, 83, (byte) 132, (byte) 141, 121};
+        }
+    }
+
+    @Test
+    void codeVerifier() {
+        assertEquals("dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk", pkceExample.codeVerifier());
+    }
+
+    @Test
+    void codeChallenge() {
+        assertEquals("E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM", pkceExample.codeChallenge());
+    }
+}
