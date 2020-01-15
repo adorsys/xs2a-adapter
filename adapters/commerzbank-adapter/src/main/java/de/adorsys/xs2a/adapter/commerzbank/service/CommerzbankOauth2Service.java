38a37cea0976e3a9dc81e6d943837352e123cd05
Artem Snisarenko <artemsnisarenko@users.noreply.github.com>
2020-01-15 10:37:09 +0200

    CommerzbankOauth2Service.baseUrl should not be static; removed unused mapper (#319)

--- adapters/commerzbank-adapter/src/main/java/de/adorsys/xs2a/adapter/commerzbank/service/CommerzbankOauth2Service.java
+++ adapters/commerzbank-adapter/src/main/java/de/adorsys/xs2a/adapter/commerzbank/service/CommerzbankOauth2Service.java
@@ -3,7 +3,6 @@ package de.adorsys.xs2a.adapter.commerzbank.service;
 import de.adorsys.xs2a.adapter.adapter.BaseOauth2Service;
 import de.adorsys.xs2a.adapter.adapter.CertificateSubjectClientIdOauth2Service;
 import de.adorsys.xs2a.adapter.adapter.PkceOauth2Service;
-import de.adorsys.xs2a.adapter.adapter.mapper.TokenResponseMapper;
 import de.adorsys.xs2a.adapter.http.HttpClient;
 import de.adorsys.xs2a.adapter.http.StringUri;
 import de.adorsys.xs2a.adapter.http.UriBuilder;
@@ -12,7 +11,6 @@ import de.adorsys.xs2a.adapter.service.PkceOauth2Extension;
 import de.adorsys.xs2a.adapter.service.Pkcs12KeyStore;
 import de.adorsys.xs2a.adapter.service.model.Aspsp;
 import de.adorsys.xs2a.adapter.service.model.TokenResponse;
-import org.mapstruct.factory.Mappers;
 
 import java.io.IOException;
 import java.net.URI;
@@ -24,20 +22,20 @@ import java.util.Map;
  */
 public class CommerzbankOauth2Service implements Oauth2Service, PkceOauth2Extension {
 
-    private final TokenResponseMapper tokenResponseMapper = Mappers.getMapper(TokenResponseMapper.class);
     private final Oauth2Service oauth2Service;
-    private static String baseUrl;
+    private final String baseUrl;
 
-    private CommerzbankOauth2Service(Oauth2Service oauth2Service) {
+    private CommerzbankOauth2Service(Oauth2Service oauth2Service, String baseUrl) {
         this.oauth2Service = oauth2Service;
+        this.baseUrl = baseUrl;
     }
 
     public static CommerzbankOauth2Service create(Aspsp aspsp, HttpClient httpClient, Pkcs12KeyStore keyStore) {
-        baseUrl = aspsp.getIdpUrl() != null ? aspsp.getIdpUrl() : aspsp.getUrl();
+        String baseUrl = aspsp.getIdpUrl() != null ? aspsp.getIdpUrl() : aspsp.getUrl();
         BaseOauth2Service baseOauth2Service = new BaseOauth2Service(aspsp, httpClient);
         CertificateSubjectClientIdOauth2Service clientIdOauth2Service =
             new CertificateSubjectClientIdOauth2Service(baseOauth2Service, keyStore);
-        return new CommerzbankOauth2Service(new PkceOauth2Service(clientIdOauth2Service));
+        return new CommerzbankOauth2Service(new PkceOauth2Service(clientIdOauth2Service), baseUrl);
     }
 
     @Override
