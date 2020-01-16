e817627d765e1b84271e2e9b2ed6550b07d6443d
berezkin88 <berezkin05@gmail.com>
2020-01-16 16:03:33 +0200

    adjust adapter regarding updated Unicredit API on Jan 16

--- adapters/unicredit-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/UnicreditAccountInformationService.java
+++ adapters/unicredit-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/UnicreditAccountInformationService.java
@@ -3,65 +3,19 @@ package de.adorsys.xs2a.adapter.service.impl;
 import de.adorsys.xs2a.adapter.adapter.BaseAccountInformationService;
 import de.adorsys.xs2a.adapter.http.ContentType;
 import de.adorsys.xs2a.adapter.http.HttpClient;
-import de.adorsys.xs2a.adapter.http.StringUri;
 import de.adorsys.xs2a.adapter.service.RequestHeaders;
 import de.adorsys.xs2a.adapter.service.RequestParams;
 import de.adorsys.xs2a.adapter.service.Response;
-import de.adorsys.xs2a.adapter.service.impl.mapper.UnicreditCreateConsentResponseMapper;
-import de.adorsys.xs2a.adapter.service.impl.mapper.UpdatePsuAuthenticationResponseUnicreditMapper;
-import de.adorsys.xs2a.adapter.service.impl.model.UnicreditStartScaProcessResponse;
 import de.adorsys.xs2a.adapter.service.model.*;
 
 import java.util.Map;
-import java.util.function.Function;
-
-import static de.adorsys.xs2a.adapter.http.ResponseHandlers.jsonResponseHandler;
 
 public class UnicreditAccountInformationService extends BaseAccountInformationService {
 
     private static final String REDIRECT_URI = "https://example.com";
-    private static final String AUTHENTICATION_CURRENT_NUMBER = "authenticationCurrentNumber";
-    private static final String VALUE = "400";
-
-    private final UnicreditCreateConsentResponseMapper createConsentResponseMapper = new UnicreditCreateConsentResponseMapper();
-    private UpdatePsuAuthenticationResponseUnicreditMapper unicreditMapper = new UpdatePsuAuthenticationResponseUnicreditMapper();
 
     public UnicreditAccountInformationService(Aspsp aspsp, HttpClient httpClient) {
         super(aspsp, httpClient);
-    }
-
-    @Override
-    public Response<ConsentCreationResponse> createConsent(RequestHeaders requestHeaders, Consents body) {
-        return createConsent(requestHeaders, body, ConsentCreationResponse.class, createConsentResponseMapper::modifyResponse);
-    }
-
-    @Override
-    public Response<UpdatePsuAuthenticationResponse> updateConsentsPsuData(String consentId, String authorisationId, RequestHeaders requestHeaders, UpdatePsuAuthentication updatePsuAuthentication) {
-        String uri = StringUri.fromElements(getConsentBaseUri(), consentId, AUTHORISATIONS, authorisationId);
-        Map<String, String> headersMap = populatePutHeaders(requestHeaders.toMap());
-        String body = jsonMapper.writeValueAsString(updatePsuAuthentication);
-
-        Response<UnicreditStartScaProcessResponse> response = httpClient.put(uri)
-            .jsonBody(body)
-            .headers(headersMap)
-            .send(jsonResponseHandler(UnicreditStartScaProcessResponse.class));
-
-        return new Response<>(response.getStatusCode(), unicreditMapper.modifyResponse(response.getBody()), response.getHeaders());
-    }
-
-    @Override
-    protected <T> Response<ScaStatusResponse> updateConsentsPsuData(String consentId, String authorisationId, RequestHeaders requestHeaders, TransactionAuthorisation transactionAuthorisation, Class<T> klass, Function<T, ScaStatusResponse> mapper) {
-        String uri = StringUri.appendQueryParam(getUpdateConsentPsuDataUri(consentId, authorisationId), AUTHENTICATION_CURRENT_NUMBER, VALUE);
-        Map<String, String> headersMap = populatePutHeaders(requestHeaders.toMap());
-        String body = jsonMapper.writeValueAsString(transactionAuthorisation);
-
-        Response<T> response = httpClient.put(uri)
-            .jsonBody(body)
-            .headers(headersMap)
-            .send(requestBuilderInterceptor, jsonResponseHandler(klass));
-
-        ScaStatusResponse scaStatusResponse = mapper.apply(response.getBody());
-        return new Response<>(response.getStatusCode(), scaStatusResponse, response.getHeaders());
     }
 
     @Override
--- adapters/unicredit-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/mapper/ScaStatusResponseMapper.java
+++ adapters/unicredit-adapter/src/main/java/de/adorsys/xs2a/adapter/service/impl/mapper/ScaStatusResponseMapper.java
@@ -1,38 +1,11 @@
 package de.adorsys.xs2a.adapter.service.impl.mapper;
 
 import de.adorsys.xs2a.adapter.service.model.ScaStatus;
-import de.adorsys.xs2a.adapter.service.model.ConsentStatus;
-import de.adorsys.xs2a.adapter.service.model.ConsentStatusResponse;
-import de.adorsys.xs2a.adapter.service.impl.model.UnicreditAccountScaStatusResponse;
 import de.adorsys.xs2a.adapter.service.impl.model.UnicreditPaymentScaStatusResponse;
 import de.adorsys.xs2a.adapter.service.model.ScaStatusResponse;
 
-import java.util.Optional;
-
 public class ScaStatusResponseMapper {
     private final ScaStatusMapper scaStatusMapper = new ScaStatusMapper();
-
-    public ScaStatusResponse toScaStatusResponse(UnicreditAccountScaStatusResponse unicreditAccountScaStatusResponse) {
-        ScaStatus scaStatus = scaStatusMapper.toScaStatus(unicreditAccountScaStatusResponse.getConsentStatus());
-
-        ScaStatusResponse scaStatusResponse = new ScaStatusResponse();
-        scaStatusResponse.setScaStatus(scaStatus);
-
-        return scaStatusResponse;
-    }
-
-    public ScaStatusResponse toScaStatusResponse(ConsentStatusResponse consentStatusResponse) {
-        ConsentStatus consentStatus = Optional.ofNullable(consentStatusResponse)
-                                          .map(ConsentStatusResponse::getConsentStatus)
-                                          .orElse(null);
-
-        ScaStatus scaStatus = scaStatusMapper.toScaStatus(consentStatus);
-
-        ScaStatusResponse scaStatusResponse = new ScaStatusResponse();
-        scaStatusResponse.setScaStatus(scaStatus);
-
-        return scaStatusResponse;
-    }
 
     public ScaStatusResponse toScaStatusResponse(UnicreditPaymentScaStatusResponse unicreditPaymentScaStatusResponse) {
         ScaStatus scaStatus = scaStatusMapper.toScaStatus(unicreditPaymentScaStatusResponse.getTransactionStatus());
