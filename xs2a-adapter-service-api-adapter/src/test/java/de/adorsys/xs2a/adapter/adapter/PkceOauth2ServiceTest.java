aefbcbad13424c45ec7e956cb1a559a5f4666726
berezkin88 <berezkin05@gmail.com>
2020-01-29 11:55:42 +0200

    cover PkceOauth2Service with tests

--- xs2a-adapter-service-api-adapter/src/test/java/de/adorsys/xs2a/adapter/adapter/PkceOauth2ServiceTest.java
+++ xs2a-adapter-service-api-adapter/src/test/java/de/adorsys/xs2a/adapter/adapter/PkceOauth2ServiceTest.java
@@ -3,10 +3,12 @@ package de.adorsys.xs2a.adapter.adapter;
 import de.adorsys.xs2a.adapter.service.Oauth2Service;
 import de.adorsys.xs2a.adapter.service.Oauth2Service.GrantType;
 import de.adorsys.xs2a.adapter.service.Oauth2Service.Parameters;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.io.IOException;
+import java.net.URI;
 
 class PkceOauth2ServiceTest {
 
@@ -33,5 +35,21 @@ class PkceOauth2ServiceTest {
 
         Mockito.verify(oauth2Service, Mockito.times(1))
             .getToken(Mockito.any(), Mockito.argThat(p -> p.getCodeVerifier() == null));
+    }
+
+    @Test
+    void getAuthorizationRequestUri() throws IOException {
+        String authorisationRequestUri = "https://authorisation.endpoint?" +
+            "response_type=code&state=state&redirect_uri=https://redirect.uri";
+        Mockito.when(oauth2Service.getAuthorizationRequestUri(Mockito.any(), Mockito.any()))
+            .thenReturn(URI.create(authorisationRequestUri));
+
+        URI actual = pkceOauth2Service.getAuthorizationRequestUri(null, parameters);
+
+        Mockito.verify(oauth2Service, Mockito.times(1))
+            .getAuthorizationRequestUri(Mockito.any(), Mockito.any());
+
+        // no way to match whole URI because codeChallenge() will always produce unique output
+        Assertions.assertTrue(actual.toString().contains(authorisationRequestUri));
     }
 }
