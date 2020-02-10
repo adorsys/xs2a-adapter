c3d78f5bfa092862bb0544fd2794d34aeace62c2
Serhii Petrychenko <39086642+spetrychenko@users.noreply.github.com>
2020-02-10 17:10:38 +0200

    add release action (#339)
    * add release action
    
    * check build number
    
    * check build number
    
    * check build number
    
    * check build number
    
    * check build number
    
    * check build number
    
    * check build number
    
    * check build number
    
    * check build number
    
    * check build number
    
    * adjust build number

--- devops/build/deploy_harbor.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-TAG=$(date +%Y%m%d%H%M)
-docker login ${HARBOR_REGISTRY} -u ${HARBOR_USER} -p ${HARBOR_TOKEN}
-docker build -t ${HARBOR_REGISTRY}/${HARBOR_NAMESPACE}/xs2a-adapter:${TAG} .
-docker push ${HARBOR_REGISTRY}/${HARBOR_NAMESPACE}/xs2a-adapter:${TAG}
