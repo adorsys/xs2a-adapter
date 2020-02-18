75d185698377493c6e029b22287bbf2484cdee2e
Alexander Berezkin <berezkin05@gmail.com>
2020-02-18 14:51:14 +0200

    Throwing BadRequestException if there are no idpUrl or ScaOAuthLink (#343)
    * add throwing BadRequestException if there are no idpUrl or ScaOAuthLink
    
    * add tests for ComdirectOAuth2Service
    
    * add release notes

--- adapters/comdirect-adapter/src/main/java/de/adorsys/xs2a/adapter/service/ComdirectOauth2Service.java
+++ adapters/comdirect-adapter/src/main/java/de/adorsys/xs2a/adapter/service/ComdirectOauth2Service.java
@@ -22,8 +22,10 @@ import de.adorsys.xs2a.adapter.adapter.PkceOauth2Service;
 import de.adorsys.xs2a.adapter.http.HttpClient;
 import de.adorsys.xs2a.adapter.http.StringUri;
 import de.adorsys.xs2a.adapter.http.UriBuilder;
+import de.adorsys.xs2a.adapter.service.exception.BadRequestException;
 import de.adorsys.xs2a.adapter.service.model.Aspsp;
 import de.adorsys.xs2a.adapter.service.model.TokenResponse;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.net.URI;
@@ -34,12 +36,17 @@ import java.util.Map;
  */
 public class ComdirectOauth2Service implements Oauth2Service, PkceOauth2Extension {
 
+    private static final String SCA_OAUTH_LINK_MISSING_ERROR_MESSAGE = "SCA OAuth link is missing or has a wrong format: " +
+        "it has to be either provided as a request parameter or preconfigured for the current ASPSP";
+
     private final Oauth2Service oauth2Service;
     private final String baseUrl;
+    private final Aspsp aspsp;
 
-    private ComdirectOauth2Service(Oauth2Service oauth2Service, String baseUrl) {
+    private ComdirectOauth2Service(Oauth2Service oauth2Service, String baseUrl, Aspsp aspsp) {
         this.oauth2Service = oauth2Service;
         this.baseUrl = baseUrl;
+        this.aspsp = aspsp;
     }
 
     public static ComdirectOauth2Service create(Aspsp aspsp, HttpClient httpClient, Pkcs12KeyStore keyStore) {
@@ -47,12 +54,17 @@ public class ComdirectOauth2Service implements Oauth2Service, PkceOauth2Extensio
         BaseOauth2Service baseOauth2Service = new BaseOauth2Service(aspsp, httpClient);
         CertificateSubjectClientIdOauth2Service clientIdOauth2Service =
             new CertificateSubjectClientIdOauth2Service(baseOauth2Service, keyStore);
-        return new ComdirectOauth2Service(new PkceOauth2Service(clientIdOauth2Service), baseUrl);
+        return new ComdirectOauth2Service(new PkceOauth2Service(clientIdOauth2Service), baseUrl, aspsp);
     }
 
     @Override
     public URI getAuthorizationRequestUri(Map<String, String> headers, Parameters parameters) throws IOException {
+        if (StringUtils.isBlank(getScaOAuthUrl(parameters))) {
+            throw new BadRequestException(SCA_OAUTH_LINK_MISSING_ERROR_MESSAGE);
+        }
+
         parameters.setAuthorizationEndpoint(parameters.removeScaOAuthLink());
+
         return UriBuilder.fromUri(oauth2Service.getAuthorizationRequestUri(headers, parameters))
             .queryParam(Parameters.SCOPE, scope(parameters))
             .build();
@@ -70,5 +82,15 @@ public class ComdirectOauth2Service implements Oauth2Service, PkceOauth2Extensio
         parameters.removeScaOAuthLink();
         parameters.setTokenEndpoint(StringUri.fromElements(baseUrl, "/v1/token"));
         return oauth2Service.getToken(headers, parameters);
+    }
+
+    private String getScaOAuthUrl(Parameters parameters) {
+        String baseScaOAuthUrl = parameters.getScaOAuthLink();
+
+        if (StringUtils.isBlank(baseScaOAuthUrl)) {
+            baseScaOAuthUrl = aspsp.getIdpUrl();
+        }
+
+        return baseScaOAuthUrl;
     }
 }
