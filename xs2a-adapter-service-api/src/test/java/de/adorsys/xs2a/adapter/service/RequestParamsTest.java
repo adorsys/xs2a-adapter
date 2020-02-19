39c93f4f44051157e08ca5014f8395f339ea562a
Artem Snisarenko <asn@adorsys.com.ua>
2020-02-19 11:11:41 +0200

    Refactor RequestParams to carry additional query parameters

--- xs2a-adapter-service-api/src/test/java/de/adorsys/xs2a/adapter/service/RequestParamsTest.java
+++ xs2a-adapter-service-api/src/test/java/de/adorsys/xs2a/adapter/service/RequestParamsTest.java
@@ -3,7 +3,9 @@ package de.adorsys.xs2a.adapter.service;
 import org.junit.jupiter.api.Test;
 
 import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonMap;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RequestParamsTest {
 
@@ -12,5 +14,25 @@ public class RequestParamsTest {
         RequestParams requestParams = RequestParams.fromMap(emptyMap());
 
         assertThat(requestParams.toMap()).isEqualTo(emptyMap());
+    }
+
+    @Test
+    void dateFromIsTypeChecked() {
+        assertThrows(IllegalArgumentException.class, () -> RequestParams.fromMap(singletonMap("dateFrom", "asdf")));
+    }
+
+    @Test
+    void dateToIsTypeChecked() {
+        assertThrows(IllegalArgumentException.class, () -> RequestParams.fromMap(singletonMap("dateTo", "asdf")));
+    }
+
+    @Test
+    void withBalanceIsTypeChecked() {
+        assertThrows(IllegalArgumentException.class, () -> RequestParams.fromMap(singletonMap("withBalance", "asdf")));
+    }
+
+    @Test
+    void deltaListIsTypeChecked() {
+        assertThrows(IllegalArgumentException.class, () -> RequestParams.fromMap(singletonMap("deltaList", "asdf")));
     }
 }
