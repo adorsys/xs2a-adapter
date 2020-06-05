89095e80e807481732d78344f6c8fc73e13a374b
Mykola Shumyn <msh@adorsys.com.ua>
2020-06-05 14:05:38 +0300

    Changed TODOs to be compliant with checkstyle requirements

--- adapters/comdirect-adapter/src/main/java/de/adorsys/xs2a/adapter/service/ComdirectOauth2Service.java
+++ adapters/comdirect-adapter/src/main/java/de/adorsys/xs2a/adapter/service/ComdirectOauth2Service.java
@@ -82,7 +82,7 @@ public class ComdirectOauth2Service extends AbstractService implements Oauth2Ser
             .build();
     }
 
-    // TODO: extract this to some service if the same logic appears for one more adapter https://jira.adorsys.de/browse/XS2AAD-548
+    // TODO extract this to some service if the same logic appears for one more adapter https://jira.adorsys.de/browse/XS2AAD-548
     @Override
     public List<ValidationError> validateGetAuthorizationRequestUri(Map<String, String> headers,
                                                                     Parameters parameters) {
