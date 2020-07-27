613147d6101b8e54ecb2b88c96ef3f2361f87f45
Petrychenko <spe@adorsys.com.ua>
2020-07-27 14:30:17 +0300

    adjust config property

--- xs2a-adapter-app/src/main/java/de/adorsys/xs2a/adapter/config/WebConfiguration.java
+++ xs2a-adapter-app/src/main/java/de/adorsys/xs2a/adapter/config/WebConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-    @Value("${audit.sanitized:true}")
+    @Value("${xs2a-adapter.audit.sanitized:true}")
     private boolean auditSanitized;
 
     @Override
