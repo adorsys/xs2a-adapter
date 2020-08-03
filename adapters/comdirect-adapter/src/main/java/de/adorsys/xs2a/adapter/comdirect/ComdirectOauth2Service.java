4e84f9930ab4dbc80566ed272c4f97c45ced5e05
Artem Snisarenko <asn@adorsys.com.ua>
2020-08-03 12:25:00 +0300

    Refactor duplicated code XS2AAD-548

--- adapters/comdirect-adapter/src/main/java/de/adorsys/xs2a/adapter/comdirect/ComdirectOauth2Service.java
+++ adapters/comdirect-adapter/src/main/java/de/adorsys/xs2a/adapter/comdirect/ComdirectOauth2Service.java
@@ -16,18 +16,17 @@
 
 package de.adorsys.xs2a.adapter.comdirect;
 
+import de.adorsys.xs2a.adapter.ScopeWithResourceIdOauth2Service;
 import de.adorsys.xs2a.adapter.adapter.BaseOauth2Service;
 import de.adorsys.xs2a.adapter.adapter.CertificateSubjectClientIdOauth2Service;
+import de.adorsys.xs2a.adapter.adapter.Oauth2ServiceDecorator;
 import de.adorsys.xs2a.adapter.adapter.PkceOauth2Service;
 import de.adorsys.xs2a.adapter.http.HttpClient;
 import de.adorsys.xs2a.adapter.http.StringUri;
-import de.adorsys.xs2a.adapter.http.UriBuilder;
 import de.adorsys.xs2a.adapter.service.Oauth2Service;
 import de.adorsys.xs2a.adapter.service.PkceOauth2Extension;
 import de.adorsys.xs2a.adapter.service.Pkcs12KeyStore;
-import de.adorsys.xs2a.adapter.service.exception.BadRequestException;
 import de.adorsys.xs2a.adapter.service.model.Aspsp;
-import de.adorsys.xs2a.adapter.service.model.Scope;
 import de.adorsys.xs2a.adapter.service.model.TokenResponse;
 import de.adorsys.xs2a.adapter.validation.ValidationError;
 import org.apache.commons.lang3.StringUtils;
@@ -44,24 +43,18 @@ import static de.adorsys.xs2a.adapter.validation.Validation.requireValid;
 /**
  * @see <a href="https://xs2a-developer.comdirect.de/content/howto/ais-manage-consents">Authorisation</a>
  */
-public class ComdirectOauth2Service implements Oauth2Service, PkceOauth2Extension {
+public class ComdirectOauth2Service extends Oauth2ServiceDecorator implements PkceOauth2Extension {
 
     private static final String SCA_OAUTH_LINK_MISSING_ERROR_MESSAGE = "SCA OAuth link is missing or has a wrong format: " +
         "it has to be either provided as a request parameter or preconfigured for the current ASPSP";
-    protected static final String UNSUPPORTED_SCOPE_VALUE_ERROR_MESSAGE = "Scope value [%s] is not supported";
-    protected static final String UNKNOWN_SCOPE_VALUE_ERROR_MESSAGE = "Unknown scope value";
-    protected static final String CONSENT_OR_PAYMENT_ID_MISSING_ERROR_MESSAGE = "Either consent id or payment id should be provided";
-    protected static final String PAYMENT_ID_MISSING_ERROR_MESSAGE = "Payment id should be provided for pis scope";
-    protected static final String CONSENT_ID_MISSING_ERROR_MESSAGE = "Consent id should be provided for ais scope";
     private static final String AIS_SCOPE_PREFIX = "AIS:";
     private static final String PIS_SCOPE_PREFIX = "PIS:";
 
-    private final Oauth2Service oauth2Service;
     private final String baseUrl;
     private final Aspsp aspsp;
 
     private ComdirectOauth2Service(Oauth2Service oauth2Service, String baseUrl, Aspsp aspsp) {
-        this.oauth2Service = oauth2Service;
+        super(oauth2Service);
         this.baseUrl = baseUrl;
         this.aspsp = aspsp;
     }
@@ -71,7 +64,10 @@ public class ComdirectOauth2Service implements Oauth2Service, PkceOauth2Extensio
         BaseOauth2Service baseOauth2Service = new BaseOauth2Service(aspsp, httpClient);
         CertificateSubjectClientIdOauth2Service clientIdOauth2Service =
             new CertificateSubjectClientIdOauth2Service(baseOauth2Service, keyStore);
-        return new ComdirectOauth2Service(new PkceOauth2Service(clientIdOauth2Service), baseUrl, aspsp);
+        PkceOauth2Service pkceOauth2Service = new PkceOauth2Service(clientIdOauth2Service);
+        ScopeWithResourceIdOauth2Service scopeOauth2Service =
+            new ScopeWithResourceIdOauth2Service(pkceOauth2Service, AIS_SCOPE_PREFIX, PIS_SCOPE_PREFIX);
+        return new ComdirectOauth2Service(scopeOauth2Service, baseUrl, aspsp);
     }
 
     @Override
@@ -80,12 +76,9 @@ public class ComdirectOauth2Service implements Oauth2Service, PkceOauth2Extensio
 
         parameters.setAuthorizationEndpoint(parameters.removeScaOAuthLink());
 
-        return UriBuilder.fromUri(oauth2Service.getAuthorizationRequestUri(headers, parameters))
-            .queryParam(Parameters.SCOPE, scope(parameters))
-            .build();
+        return oauth2Service.getAuthorizationRequestUri(headers, parameters);
     }
 
-    // TODO extract this to some service if the same logic appears for one more adapter https://jira.adorsys.de/browse/XS2AAD-548
     @Override
     public List<ValidationError> validateGetAuthorizationRequestUri(Map<String, String> headers,
                                                                     Parameters parameters) {
@@ -97,55 +90,7 @@ public class ComdirectOauth2Service implements Oauth2Service, PkceOauth2Extensio
                 SCA_OAUTH_LINK_MISSING_ERROR_MESSAGE));
         }
 
-        String scope = parameters.getScope();
-        if (StringUtils.isNotEmpty(scope)) {
-            if (!Scope.contains(scope)) {
-                validationErrors.add(new ValidationError(ValidationError.Code.NOT_SUPPORTED,
-                    Parameters.SCOPE,
-                    String.format(UNSUPPORTED_SCOPE_VALUE_ERROR_MESSAGE, scope)));
-            } else if (Scope.isAis(Scope.fromValue(scope))) {
-                if (StringUtils.isBlank(parameters.getConsentId())) {
-                    validationErrors.add(new ValidationError(ValidationError.Code.REQUIRED,
-                        Parameters.CONSENT_ID,
-                        CONSENT_ID_MISSING_ERROR_MESSAGE));
-                }
-            } else if (Scope.isPis(Scope.fromValue(scope)) && StringUtils.isBlank(parameters.getPaymentId())) {
-                validationErrors.add(new ValidationError(ValidationError.Code.REQUIRED,
-                    Parameters.PAYMENT_ID,
-                    PAYMENT_ID_MISSING_ERROR_MESSAGE));
-            }
-        }
-
         return Collections.unmodifiableList(validationErrors);
-    }
-
-    private String scope(Parameters parameters) {
-        if (StringUtils.isEmpty(parameters.getScope())) {
-            return computeScope(parameters);
-        } else {
-            return mapScope(parameters);
-        }
-    }
-
-    private String computeScope(Parameters parameters) {
-        if (StringUtils.isNotBlank(parameters.getConsentId())) {
-            return AIS_SCOPE_PREFIX + parameters.getConsentId();
-        }
-        if (StringUtils.isNotBlank(parameters.getPaymentId())) {
-            return PIS_SCOPE_PREFIX + parameters.getPaymentId();
-        }
-        throw new BadRequestException(CONSENT_OR_PAYMENT_ID_MISSING_ERROR_MESSAGE);
-    }
-
-    private String mapScope(Parameters parameters) {
-        Scope scope = Scope.fromValue(parameters.getScope());
-
-        if (Scope.isAis(scope)) {
-            return AIS_SCOPE_PREFIX + parameters.getConsentId();
-        } else if (Scope.isPis(scope)) {
-            return PIS_SCOPE_PREFIX + parameters.getPaymentId();
-        }
-        throw new BadRequestException(UNKNOWN_SCOPE_VALUE_ERROR_MESSAGE);
     }
 
     @Override
