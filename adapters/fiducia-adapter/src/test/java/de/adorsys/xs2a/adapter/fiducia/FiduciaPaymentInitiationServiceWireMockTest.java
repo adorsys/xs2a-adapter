f413dcecb3c161e92e424a96a0d0f3a5de953d0b
berezkin88 <berezkin05@gmail.com>
2020-09-10 10:51:10 +0300

    record pis periodic pain001

--- adapters/fiducia-adapter/src/test/java/de/adorsys/xs2a/adapter/fiducia/FiduciaPaymentInitiationServiceWireMockTest.java
+++ adapters/fiducia-adapter/src/test/java/de/adorsys/xs2a/adapter/fiducia/FiduciaPaymentInitiationServiceWireMockTest.java
@@ -6,15 +6,20 @@ import de.adorsys.xs2a.adapter.api.Response;
 import de.adorsys.xs2a.adapter.api.model.*;
 import de.adorsys.xs2a.adapter.test.ServiceWireMockTest;
 import de.adorsys.xs2a.adapter.test.TestRequestResponse;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import static de.adorsys.xs2a.adapter.api.model.PaymentProduct.PAIN_001_SEPA_CREDIT_TRANSFERS;
 import static de.adorsys.xs2a.adapter.api.model.PaymentService.PAYMENTS;
+import static de.adorsys.xs2a.adapter.api.model.PaymentService.PERIODIC_PAYMENTS;
+import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
@@ -22,36 +27,50 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 public class FiduciaPaymentInitiationServiceWireMockTest {
     protected static final String PAYMENTS_PAYMENT_ID = "9667011219090020282PSDDE-BAFIN-911360PA4960JJ";
     protected static final String PAYMENTS_AUTHORISATION_ID = "9592111219090020283PSDDE-BAFIN-911360AU4960JJ";
+    protected static final String PERIODIC_PAYMENT_ID = "2750410619090020256PSDDE-BAFIN-911360PA4960JJ";
+    protected static final String PERIODIC_AUTHORISATION_ID = "0509410619090020257PSDDE-BAFIN-911360AU4960JJ";
 
     private final PaymentInitiationService service;
+
+    private static final Ids paymentsIds = getPaymentIds();
+    private static final Ids authorisationIds = getAuthorisationIds();
 
     public FiduciaPaymentInitiationServiceWireMockTest(PaymentInitiationService service) {
         this.service = service;
     }
 
-    @Test
-    void initiatePayment() throws Exception {
+    @ParameterizedTest
+    @MethodSource("paymentTypes")
+    void initiatePayment(PaymentService paymentService, PaymentProduct paymentProduct) throws Exception {
         TestRequestResponse requestResponse =
-            new TestRequestResponse("pis/payments/pain.001-sepa-credit-transfers/initiate-payment.json");
+            new TestRequestResponse(format("pis/%s/%s/initiate-payment.json", paymentService, paymentProduct));
 
-        Response<PaymentInitationRequestResponse201> response = service.initiatePayment(PAYMENTS,
-            PAIN_001_SEPA_CREDIT_TRANSFERS,
+        Response<PaymentInitationRequestResponse201> response = service.initiatePayment(paymentService,
+            paymentProduct,
             requestResponse.requestHeaders(),
             RequestParams.empty(),
-            requestResponse.requestBody());
+            resolveRequestBody(requestResponse, paymentService));
 
         assertThat(response.getBody()).isEqualTo(requestResponse.responseBody(PaymentInitationRequestResponse201.class));
+    }
+
+    private Object resolveRequestBody(TestRequestResponse requestResponse, PaymentService paymentService) {
+        if (paymentService == PERIODIC_PAYMENTS) {
+            return requestResponse.requestBody(PeriodicPaymentInitiationMultipartBody.class);
+        }
+
+        return requestResponse.requestBody();
     }
 
     @ParameterizedTest
     @MethodSource("paymentTypes")
     void authenticatePsu(PaymentService paymentService, PaymentProduct paymentProduct) throws Exception {
         TestRequestResponse requestResponse =
-            new TestRequestResponse("pis/" + paymentService + "/" + paymentProduct + "/authenticate-psu.json");
+            new TestRequestResponse(format("pis/%s/%s/authenticate-psu.json", paymentService, paymentProduct));
 
         Response<StartScaprocessResponse> response = service.startPaymentAuthorisation(paymentService,
             paymentProduct,
-            PAYMENTS_PAYMENT_ID,
+            paymentsIds.get(paymentService, paymentProduct),
             requestResponse.requestHeaders(),
             RequestParams.empty(),
             requestResponse.requestBody(UpdatePsuAuthentication.class));
@@ -63,12 +82,12 @@ public class FiduciaPaymentInitiationServiceWireMockTest {
     @MethodSource("paymentTypes")
     void selectScaMethod(PaymentService paymentService, PaymentProduct paymentProduct) throws Exception {
         TestRequestResponse requestResponse =
-            new TestRequestResponse("pis/" + paymentService + "/" + paymentProduct + "/select-sca-method.json");
+            new TestRequestResponse(format("pis/%s/%s/select-sca-method.json", paymentService, paymentProduct));
 
         Response<SelectPsuAuthenticationMethodResponse> response = service.updatePaymentPsuData(paymentService,
             paymentProduct,
-            PAYMENTS_PAYMENT_ID,
-            PAYMENTS_AUTHORISATION_ID,
+            paymentsIds.get(paymentService, paymentProduct),
+            authorisationIds.get(paymentService, paymentProduct),
             requestResponse.requestHeaders(),
             RequestParams.empty(),
             requestResponse.requestBody(SelectPsuAuthenticationMethod.class));
@@ -80,12 +99,12 @@ public class FiduciaPaymentInitiationServiceWireMockTest {
     @MethodSource("paymentTypes")
     void authoriseTransaction(PaymentService paymentService, PaymentProduct paymentProduct) throws Exception {
         TestRequestResponse requestResponse =
-            new TestRequestResponse("pis/" + paymentService + "/" + paymentProduct + "/authorise-transaction.json");
+            new TestRequestResponse(format("pis/%s/%s/authorise-transaction.json", paymentService, paymentProduct));
 
         Response<ScaStatusResponse> response = service.updatePaymentPsuData(paymentService,
             paymentProduct,
-            PAYMENTS_PAYMENT_ID,
-            PAYMENTS_AUTHORISATION_ID,
+            paymentsIds.get(paymentService, paymentProduct),
+            authorisationIds.get(paymentService, paymentProduct),
             requestResponse.requestHeaders(),
             RequestParams.empty(),
             requestResponse.requestBody(TransactionAuthorisation.class));
@@ -97,11 +116,11 @@ public class FiduciaPaymentInitiationServiceWireMockTest {
     @MethodSource("paymentTypes")
     void getPaymentStatus(PaymentService paymentService, PaymentProduct paymentProduct) throws Exception {
         TestRequestResponse requestResponse =
-            new TestRequestResponse("pis/" + paymentService + "/" + paymentProduct + "/get-payment-status.json");
+            new TestRequestResponse(format("pis/%s/%s/get-payment-status.json", paymentService, paymentProduct));
 
         Response<String> response = service.getPaymentInitiationStatusAsString(paymentService,
             paymentProduct,
-            PAYMENTS_PAYMENT_ID,
+            paymentsIds.get(paymentService, paymentProduct),
             requestResponse.requestHeaders(),
             RequestParams.empty());
 
@@ -112,12 +131,12 @@ public class FiduciaPaymentInitiationServiceWireMockTest {
     @MethodSource("paymentTypes")
     void getScaStatus(PaymentService paymentService, PaymentProduct paymentProduct) throws Exception {
         TestRequestResponse requestResponse =
-            new TestRequestResponse("pis/" + paymentService + "/" + paymentProduct + "/get-sca-status.json");
+            new TestRequestResponse(format("pis/%s/%s/get-sca-status.json", paymentService, paymentProduct));
 
         Response<ScaStatusResponse> response = service.getPaymentInitiationScaStatus(paymentService,
             paymentProduct,
-            PAYMENTS_PAYMENT_ID,
-            PAYMENTS_AUTHORISATION_ID,
+            paymentsIds.get(paymentService, paymentProduct),
+            authorisationIds.get(paymentService, paymentProduct),
             requestResponse.requestHeaders(),
             RequestParams.empty());
 
@@ -125,6 +144,35 @@ public class FiduciaPaymentInitiationServiceWireMockTest {
     }
 
     private static Stream<Arguments> paymentTypes() {
-        return Stream.of(arguments(PAYMENTS, PAIN_001_SEPA_CREDIT_TRANSFERS));
+        return Stream.of(arguments(PAYMENTS, PAIN_001_SEPA_CREDIT_TRANSFERS),
+            arguments(PERIODIC_PAYMENTS, PAIN_001_SEPA_CREDIT_TRANSFERS));
+    }
+
+    private static Ids getPaymentIds() {
+        return new Ids()
+            .add(PAYMENTS, PAIN_001_SEPA_CREDIT_TRANSFERS, PAYMENTS_PAYMENT_ID)
+            .add(PERIODIC_PAYMENTS, PAIN_001_SEPA_CREDIT_TRANSFERS, PERIODIC_PAYMENT_ID);
+    }
+
+    private static Ids getAuthorisationIds() {
+        return new Ids()
+            .add(PAYMENTS, PAIN_001_SEPA_CREDIT_TRANSFERS, PAYMENTS_AUTHORISATION_ID)
+            .add(PERIODIC_PAYMENTS, PAIN_001_SEPA_CREDIT_TRANSFERS, PERIODIC_AUTHORISATION_ID);
+    }
+
+    private static class Ids {
+        private final Map<PaymentService, Map<PaymentProduct, String>> map = new EnumMap<>(PaymentService.class);
+
+        Ids add(PaymentService paymentService, PaymentProduct paymentProduct, String id) {
+            map.computeIfAbsent(paymentService, k -> new EnumMap<>(PaymentProduct.class))
+                .put(paymentProduct, id);
+            return this;
+        }
+
+        String get(PaymentService paymentService, PaymentProduct paymentProduct) {
+            return Optional.ofNullable(map.get(paymentService))
+                .map(m -> m.get(paymentProduct))
+                .orElseThrow(NoSuchElementException::new);
+        }
     }
 }
