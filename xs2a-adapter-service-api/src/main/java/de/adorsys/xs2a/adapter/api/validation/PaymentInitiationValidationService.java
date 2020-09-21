9b8a67c4870885f7d166239bb93ee17aa2642c3e
Artem Snisarenko <artemsnisarenko@users.noreply.github.com>
2020-09-21 11:09:33 +0300

    Remove deprecated PIS methods (#508)
    Co-authored-by: Artem Snisarenko <asn@adorsys.com.ua>

--- xs2a-adapter-service-api/src/main/java/de/adorsys/xs2a/adapter/api/validation/PaymentInitiationValidationService.java
+++ xs2a-adapter-service-api/src/main/java/de/adorsys/xs2a/adapter/api/validation/PaymentInitiationValidationService.java
@@ -8,21 +8,6 @@ import java.util.Collections;
 import java.util.List;
 
 public interface PaymentInitiationValidationService {
-    /**
-     * @deprecated use the version with enum PaymentService/Product parameters
-     */
-    @Deprecated
-    default List<ValidationError> validateInitiatePayment(String paymentService,
-                                                          String paymentProduct,
-                                                          RequestHeaders requestHeaders,
-                                                          RequestParams requestParams,
-                                                          Object body) {
-        return validateInitiatePayment(PaymentService.fromValue(paymentService),
-            PaymentProduct.fromValue(paymentProduct),
-            requestHeaders,
-            requestParams,
-            body);
-    }
 
     default List<ValidationError> validateInitiatePayment(PaymentService paymentService,
                                                           PaymentProduct paymentProduct,
@@ -32,39 +17,11 @@ public interface PaymentInitiationValidationService {
         return Collections.emptyList();
     }
 
-    /**
-     * @deprecated use the version with enum PaymentProduct parameter
-     */
-    @Deprecated
-    default List<ValidationError> validateGetSinglePaymentInformation(String paymentProduct,
-                                                                      String paymentId,
-                                                                      RequestHeaders requestHeaders,
-                                                                      RequestParams requestParams) {
-        return validateGetSinglePaymentInformation(PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            requestHeaders,
-            requestParams);
-    }
-
     default List<ValidationError> validateGetSinglePaymentInformation(PaymentProduct paymentProduct,
                                                                       String paymentId,
                                                                       RequestHeaders requestHeaders,
                                                                       RequestParams requestParams) {
         return Collections.emptyList();
-    }
-
-    /**
-     * @deprecated use the version with enum PaymentProduct parameter
-     */
-    @Deprecated
-    default List<ValidationError> validateGetPeriodicPaymentInformation(String paymentProduct,
-                                                                        String paymentId,
-                                                                        RequestHeaders requestHeaders,
-                                                                        RequestParams requestParams) {
-        return validateGetPeriodicPaymentInformation(PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            requestHeaders,
-            requestParams);
     }
 
     default List<ValidationError> validateGetPeriodicPaymentInformation(PaymentProduct paymentProduct,
@@ -74,43 +31,11 @@ public interface PaymentInitiationValidationService {
         return Collections.emptyList();
     }
 
-    /**
-     * @deprecated use the version with enum PaymentProduct parameter
-     */
-    @Deprecated
-    default List<ValidationError> validateGetPeriodicPain001PaymentInformation(String paymentProduct,
-                                                                               String paymentId,
-                                                                               RequestHeaders requestHeaders,
-                                                                               RequestParams requestParams) {
-        return validateGetPeriodicPain001PaymentInformation(PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            requestHeaders,
-            requestParams);
-    }
-
     default List<ValidationError> validateGetPeriodicPain001PaymentInformation(PaymentProduct paymentProduct,
                                                                                String paymentId,
                                                                                RequestHeaders requestHeaders,
                                                                                RequestParams requestParams) {
         return Collections.emptyList();
-    }
-
-    /**
-     * @deprecated use the version with enum PaymentService/Product parameters
-     */
-    @Deprecated
-    default List<ValidationError> validateGetPaymentInitiationScaStatus(String paymentService,
-                                                                        String paymentProduct,
-                                                                        String paymentId,
-                                                                        String authorisationId,
-                                                                        RequestHeaders requestHeaders,
-                                                                        RequestParams requestParams) {
-        return validateGetPaymentInitiationScaStatus(PaymentService.fromValue(paymentService),
-            PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            authorisationId,
-            requestHeaders,
-            requestParams);
     }
 
     default List<ValidationError> validateGetPaymentInitiationScaStatus(PaymentService paymentService,
@@ -122,45 +47,12 @@ public interface PaymentInitiationValidationService {
         return Collections.emptyList();
     }
 
-    /**
-     * @deprecated use the version with enum PaymentService/Product parameters
-     */
-    @Deprecated
-    default List<ValidationError> validateGetPaymentInitiationStatus(String paymentService,
-                                                                     String paymentProduct,
-                                                                     String paymentId,
-                                                                     RequestHeaders requestHeaders,
-                                                                     RequestParams requestParams) {
-        return validateGetPaymentInitiationStatus(PaymentService.fromValue(paymentService),
-            PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            requestHeaders,
-            requestParams);
-    }
-
     default List<ValidationError> validateGetPaymentInitiationStatus(PaymentService paymentService,
                                                                      PaymentProduct paymentProduct,
                                                                      String paymentId,
                                                                      RequestHeaders requestHeaders,
                                                                      RequestParams requestParams) {
         return Collections.emptyList();
-    }
-
-
-    /**
-     * @deprecated use the version with enum PaymentService/Product parameters
-     */
-    @Deprecated
-    default List<ValidationError> validateGetPaymentInitiationStatusAsString(String paymentService,
-                                                                             String paymentProduct,
-                                                                             String paymentId,
-                                                                             RequestHeaders requestHeaders,
-                                                                             RequestParams requestParams) {
-        return validateGetPaymentInitiationStatusAsString(PaymentService.fromValue(paymentService),
-            PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            requestHeaders,
-            requestParams);
     }
 
     default List<ValidationError> validateGetPaymentInitiationStatusAsString(PaymentService paymentService,
@@ -171,22 +63,6 @@ public interface PaymentInitiationValidationService {
         return Collections.emptyList();
     }
 
-    /**
-     * @deprecated use the version with enum PaymentService/Product parameters
-     */
-    @Deprecated
-    default List<ValidationError> validateGetPaymentInitiationAuthorisation(String paymentService,
-                                                                            String paymentProduct,
-                                                                            String paymentId,
-                                                                            RequestHeaders requestHeaders,
-                                                                            RequestParams requestParams) {
-        return validateGetPaymentInitiationAuthorisation(PaymentService.fromValue(paymentService),
-            PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            requestHeaders,
-            requestParams);
-    }
-
     default List<ValidationError> validateGetPaymentInitiationAuthorisation(PaymentService paymentService,
                                                                             PaymentProduct paymentProduct,
                                                                             String paymentId,
@@ -195,46 +71,12 @@ public interface PaymentInitiationValidationService {
         return Collections.emptyList();
     }
 
-    /**
-     * @deprecated use the version with enum PaymentService/Product parameters
-     */
-    @Deprecated
-    default List<ValidationError> validateStartPaymentAuthorisation(String paymentService,
-                                                                    String paymentProduct,
-                                                                    String paymentId,
-                                                                    RequestHeaders requestHeaders,
-                                                                    RequestParams requestParams) {
-        return validateStartPaymentAuthorisation(PaymentService.fromValue(paymentService),
-            PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            requestHeaders,
-            requestParams);
-    }
-
     default List<ValidationError> validateStartPaymentAuthorisation(PaymentService paymentService,
                                                                     PaymentProduct paymentProduct,
                                                                     String paymentId,
                                                                     RequestHeaders requestHeaders,
                                                                     RequestParams requestParams) {
         return Collections.emptyList();
-    }
-
-    /**
-     * @deprecated use the version with enum PaymentService/Product parameters
-     */
-    @Deprecated
-    default List<ValidationError> validateStartPaymentAuthorisation(String paymentService,
-                                                                    String paymentProduct,
-                                                                    String paymentId,
-                                                                    RequestHeaders requestHeaders,
-                                                                    RequestParams requestParams,
-                                                                    UpdatePsuAuthentication body) {
-        return validateStartPaymentAuthorisation(PaymentService.fromValue(paymentService),
-            PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            requestHeaders,
-            requestParams,
-            body);
     }
 
     default List<ValidationError> validateStartPaymentAuthorisation(PaymentService paymentService,
@@ -244,26 +86,6 @@ public interface PaymentInitiationValidationService {
                                                                     RequestParams requestParams,
                                                                     UpdatePsuAuthentication body) {
         return Collections.emptyList();
-    }
-
-    /**
-     * @deprecated use the version with enum PaymentService/Product parameters
-     */
-    @Deprecated
-    default List<ValidationError> validateUpdatePaymentPsuData(String paymentService,
-                                                               String paymentProduct,
-                                                               String paymentId,
-                                                               String authorisationId,
-                                                               RequestHeaders requestHeaders,
-                                                               RequestParams requestParams,
-                                                               SelectPsuAuthenticationMethod body) {
-        return validateUpdatePaymentPsuData(PaymentService.fromValue(paymentService),
-            PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            authorisationId,
-            requestHeaders,
-            requestParams,
-            body);
     }
 
     default List<ValidationError> validateUpdatePaymentPsuData(PaymentService paymentService,
@@ -276,26 +98,6 @@ public interface PaymentInitiationValidationService {
         return Collections.emptyList();
     }
 
-    /**
-     * @deprecated use the version with enum PaymentService/Product parameters
-     */
-    @Deprecated
-    default List<ValidationError> validateUpdatePaymentPsuData(String paymentService,
-                                                               String paymentProduct,
-                                                               String paymentId,
-                                                               String authorisationId,
-                                                               RequestHeaders requestHeaders,
-                                                               RequestParams requestParams,
-                                                               TransactionAuthorisation body) {
-        return validateUpdatePaymentPsuData(PaymentService.fromValue(paymentService),
-            PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            authorisationId,
-            requestHeaders,
-            requestParams,
-            body);
-    }
-
     default List<ValidationError> validateUpdatePaymentPsuData(PaymentService paymentService,
                                                                PaymentProduct paymentProduct,
                                                                String paymentId,
@@ -304,26 +106,6 @@ public interface PaymentInitiationValidationService {
                                                                RequestParams requestParams,
                                                                TransactionAuthorisation body) {
         return Collections.emptyList();
-    }
-
-    /**
-     * @deprecated use the version with enum PaymentService/Product parameters
-     */
-    @Deprecated
-    default List<ValidationError> validateUpdatePaymentPsuData(String paymentService,
-                                                               String paymentProduct,
-                                                               String paymentId,
-                                                               String authorisationId,
-                                                               RequestHeaders requestHeaders,
-                                                               RequestParams requestParams,
-                                                               UpdatePsuAuthentication body) {
-        return validateUpdatePaymentPsuData(PaymentService.fromValue(paymentService),
-            PaymentProduct.fromValue(paymentProduct),
-            paymentId,
-            authorisationId,
-            requestHeaders,
-            requestParams,
-            body);
     }
 
     default List<ValidationError> validateUpdatePaymentPsuData(PaymentService paymentService,
