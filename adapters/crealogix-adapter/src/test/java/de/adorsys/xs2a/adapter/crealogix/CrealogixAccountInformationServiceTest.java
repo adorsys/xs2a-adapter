d77f2c9875ffe93661f8bd545b09eeae5a5a4922
berezkin88 <berezkin05@gmail.com>
2021-01-12 10:29:49 +0200

    resolved AI Service

--- adapters/crealogix-adapter/src/test/java/de/adorsys/xs2a/adapter/crealogix/CrealogixAccountInformationServiceTest.java
+++ adapters/crealogix-adapter/src/test/java/de/adorsys/xs2a/adapter/crealogix/CrealogixAccountInformationServiceTest.java
@@ -2,6 +2,7 @@ package de.adorsys.xs2a.adapter.crealogix;
 
 import de.adorsys.xs2a.adapter.api.*;
 import de.adorsys.xs2a.adapter.api.http.HttpClient;
+import de.adorsys.xs2a.adapter.api.http.Request;
 import de.adorsys.xs2a.adapter.api.link.LinksRewriter;
 import de.adorsys.xs2a.adapter.api.model.*;
 import de.adorsys.xs2a.adapter.impl.http.RequestBuilderImpl;
@@ -23,6 +24,8 @@ class CrealogixAccountInformationServiceTest {
     private static final String AUTHORIZATION_VALUE = "Bearer foo";
     private static final String URI = "https://foo.boo";
     public static final String ACCOUNT_ID = "accountId";
+    public static final String CONSENT_ID = "consentId";
+    public static final String AUTHORISATION_ID = "authorisationId";
     private static final String REMITTANCE_INFORMATION_STRUCTURED = "remittanceInformationStructuredStringValue";
     private final HttpClient httpClient = mock(HttpClient.class);
     private final LinksRewriter linksRewriter = mock(LinksRewriter.class);
@@ -36,7 +39,7 @@ class CrealogixAccountInformationServiceTest {
 
     @Test
     void createConsent() {
-        when(httpClient.post(anyString())).thenReturn(new RequestBuilderImpl(httpClient, "POST", URI));
+        when(httpClient.post(anyString())).thenReturn(getRequestBuilder("POST"));
         when(httpClient.send(any(), any()))
             .thenReturn(new Response<>(-1,
                                        new ConsentsResponse201(),
@@ -71,7 +74,7 @@ class CrealogixAccountInformationServiceTest {
             "}";
 
         when(httpClient.get(anyString()))
-            .thenReturn(new RequestBuilderImpl(httpClient, "GET", URI));
+            .thenReturn(getRequestBuilder("GET"));
         when(httpClient.send(any(), any()))
             .thenAnswer(invocationOnMock -> {
                 HttpClient.ResponseHandler responseHandler = invocationOnMock.getArgument(1, HttpClient.ResponseHandler.class);
@@ -113,7 +116,7 @@ class CrealogixAccountInformationServiceTest {
             "}";
 
         when(httpClient.get(anyString()))
-            .thenReturn(new RequestBuilderImpl(httpClient, "GET", URI));
+            .thenReturn(getRequestBuilder("GET"));
         when(httpClient.send(any(), any()))
             .thenAnswer(invocationOnMock -> {
                 HttpClient.ResponseHandler responseHandler = invocationOnMock.getArgument(1, HttpClient.ResponseHandler.class);
@@ -147,5 +150,260 @@ class CrealogixAccountInformationServiceTest {
         Aspsp aspsp = new Aspsp();
         aspsp.setUrl("https://url.com");
         return aspsp;
+    }
+
+    @Test
+    void getConsentInformation() {
+        when(httpClient.get(anyString()))
+            .thenReturn(getRequestBuilder("GET"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new ConsentInformationResponse200Json()));
+
+        Response<ConsentInformationResponse200Json> actualResponse = service.getConsentInformation(CONSENT_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    private <T> Response<T> getResponse(T body) {
+        return new Response<>(-1, body, ResponseHeaders.emptyResponseHeaders());
+    }
+
+    private Request.Builder getRequestBuilder(String method) {
+        return new RequestBuilderImpl(httpClient, method, URI);
+    }
+
+    @Test
+    void deleteConsent() {
+        when(httpClient.delete(anyString()))
+            .thenReturn(getRequestBuilder("DELETE"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(Void.TYPE));
+
+        Response<Void> actualResponse = service.deleteConsent(CONSENT_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void getConsentStatus() {
+        when(httpClient.get(anyString()))
+            .thenReturn(getRequestBuilder("GET"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new ConsentStatusResponse200()));
+
+        Response<ConsentStatusResponse200> actualResponse = service.getConsentStatus(CONSENT_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void startConsentAuthorisation() {
+        when(httpClient.post(anyString()))
+            .thenReturn(getRequestBuilder("POST"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new StartScaprocessResponse()));
+
+        Response<StartScaprocessResponse> actualResponse = service.startConsentAuthorisation(CONSENT_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void startConsentAuthorisationWithPsuAuthentication() {
+        when(httpClient.post(anyString()))
+            .thenReturn(getRequestBuilder("POST"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new StartScaprocessResponse()));
+
+        Response<StartScaprocessResponse> actualResponse = service.startConsentAuthorisation(CONSENT_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty(),
+            new UpdatePsuAuthentication());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void updateConsentsPsuData_authenticatePsu() {
+        when(httpClient.put(anyString()))
+            .thenReturn(getRequestBuilder("PUT"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new UpdatePsuAuthenticationResponse()));
+
+        Response<UpdatePsuAuthenticationResponse> actualResponse = service.updateConsentsPsuData(CONSENT_ID,
+            AUTHORISATION_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty(),
+            new UpdatePsuAuthentication());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void updateConsentsPsuData_selectScaMethod() {
+        when(httpClient.put(anyString()))
+            .thenReturn(getRequestBuilder("PUT"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new SelectPsuAuthenticationMethodResponse()));
+
+        Response<SelectPsuAuthenticationMethodResponse> actualResponse = service.updateConsentsPsuData(CONSENT_ID,
+            AUTHORISATION_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty(),
+            new SelectPsuAuthenticationMethod());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void updateConsentsPsuData_authorizeTransaction() {
+        when(httpClient.put(anyString()))
+            .thenReturn(getRequestBuilder("PUT"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new ScaStatusResponse()));
+
+        Response<ScaStatusResponse> actualResponse = service.updateConsentsPsuData(CONSENT_ID,
+            AUTHORISATION_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty(),
+            new TransactionAuthorisation());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void getAccountList() {
+        when(httpClient.get(anyString()))
+            .thenReturn(getRequestBuilder("GET"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new AccountList()));
+
+        Response<AccountList> actualResponse = service.getAccountList(
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void getTransactionListAsString() {
+        when(httpClient.get(anyString()))
+            .thenReturn(getRequestBuilder("GET"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse("transactions"));
+
+        Response<String> actualResponse = service.getTransactionListAsString(ACCOUNT_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void getConsentScaStatus() {
+        when(httpClient.get(anyString()))
+            .thenReturn(getRequestBuilder("GET"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new ScaStatusResponse()));
+
+        Response<String> actualResponse = service.getTransactionListAsString(ACCOUNT_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void getBalances() {
+        when(httpClient.get(anyString()))
+            .thenReturn(getRequestBuilder("GET"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new ReadAccountBalanceResponse200()));
+
+        Response<ReadAccountBalanceResponse200> actualResponse = service.getBalances(ACCOUNT_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void getCardAccountList() {
+        when(httpClient.get(anyString()))
+            .thenReturn(getRequestBuilder("GET"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new CardAccountList()));
+
+        Response<CardAccountList> actualResponse = service.getCardAccountList(
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void getCardAccountDetails() {
+        when(httpClient.get(anyString()))
+            .thenReturn(getRequestBuilder("GET"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new OK200CardAccountDetails()));
+
+        Response<OK200CardAccountDetails> actualResponse = service.getCardAccountDetails(ACCOUNT_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void getCardAccountBalances() {
+        when(httpClient.get(anyString()))
+            .thenReturn(getRequestBuilder("GET"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new ReadCardAccountBalanceResponse200()));
+
+        Response<ReadCardAccountBalanceResponse200> actualResponse = service.getCardAccountBalances(ACCOUNT_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
+    }
+
+    @Test
+    void getCardAccountTransactionList() {
+        when(httpClient.get(anyString()))
+            .thenReturn(getRequestBuilder("GET"));
+        when(httpClient.send(any(), any()))
+            .thenReturn(getResponse(new CardAccountsTransactionsResponse200()));
+
+        Response<CardAccountsTransactionsResponse200> actualResponse = service.getCardAccountTransactionList(ACCOUNT_ID,
+            RequestHeaders.fromMap(singletonMap(AUTHORIZATION, AUTHORIZATION_VALUE)),
+            RequestParams.empty());
+
+        assertThat(actualResponse)
+            .isNotNull();
     }
 }
