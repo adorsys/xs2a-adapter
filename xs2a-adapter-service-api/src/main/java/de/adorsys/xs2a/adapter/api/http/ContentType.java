dea09384c68602bdea0d0e3712bdcf6055e7ef1f
Alexander Berezkin <berezkin05@gmail.com>
2021-02-02 12:28:41 +0200

    verlag will provide application/json for getTransactionListAsString, still allows text/plain for sandbox (#559)

--- xs2a-adapter-service-api/src/main/java/de/adorsys/xs2a/adapter/api/http/ContentType.java
+++ xs2a-adapter-service-api/src/main/java/de/adorsys/xs2a/adapter/api/http/ContentType.java
@@ -7,4 +7,6 @@ public class ContentType {
     public static final String APPLICATION_JSON = "application/json";
     public static final String APPLICATION_XML = "application/xml";
     public static final String MULTIPART_FORM_DATA = "multipart/form-data";
+    public static final String ALL = "*/*";
+    public static final String TEXT_PLAIN = "text/plain";
 }
