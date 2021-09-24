346f1e42dc53116bcc669b635aeb861792565d80
berezkin88 <berezkin05@gmail.com>
2021-09-24 13:06:22 +0300

    added ais stubs for comdirect

---
+++ adapters/comdirect-adapter/src/test/java/de/adorsys/xs2a/adapter/comdirect/ComdirectOauth2ServiceWireMockTest.java
@@ -0,0 +1,36 @@
+package de.adorsys.xs2a.adapter.comdirect;
+
+import de.adorsys.xs2a.adapter.api.Oauth2Service;
+import de.adorsys.xs2a.adapter.api.model.TokenResponse;
+import de.adorsys.xs2a.adapter.test.ServiceWireMockTest;
+import de.adorsys.xs2a.adapter.test.TestRequestResponse;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ServiceWireMockTest(ComdirectServiceProvider.class)
+class ComdirectOauth2ServiceWireMockTest {
+
+    private final Oauth2Service oauth2Service;
+
+    ComdirectOauth2ServiceWireMockTest(Oauth2Service oauth2Service) {
+        this.oauth2Service = oauth2Service;
+    }
+
+    @Test
+    void getAccessToken() throws IOException {
+        var requestResponse = new TestRequestResponse("oauth2/get-access-token.json");
+
+        var modifiableParams = new LinkedHashMap<>(requestResponse.requestParams().toMap());
+
+        var actualToken = oauth2Service.getToken(requestResponse.requestHeaders().toMap(),
+            new Oauth2Service.Parameters(modifiableParams));
+
+        assertThat(actualToken)
+            .isNotNull()
+            .isEqualTo(requestResponse.responseBody(TokenResponse.class));
+    }
+}
