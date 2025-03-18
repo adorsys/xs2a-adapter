70e47b847278bc71c68ae8707920faad9ff87c14
kouamschekina <kouamdelphine82@gmail.com>
2025-03-18 08:29:39 +0100

    chore(pom): add dependency and plugin versions as properties in parent POM

--- adapters/deutsche-bank-adapter/src/main/java/de/adorsys/xs2a/adapter/deutschebank/DeutscheBankPsuPasswordEncryptionService.java
+++ adapters/deutsche-bank-adapter/src/main/java/de/adorsys/xs2a/adapter/deutschebank/DeutscheBankPsuPasswordEncryptionService.java
@@ -28,7 +28,6 @@ import de.adorsys.xs2a.adapter.api.exception.PsuPasswordEncodingException;
 import org.apache.commons.io.IOUtils;
 import org.bouncycastle.jcajce.provider.asymmetric.x509.CertificateFactory;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -76,15 +75,15 @@ public class DeutscheBankPsuPasswordEncryptionService implements PsuPasswordEncr
         CertificateFactory certificateFactory = new CertificateFactory();
 
         try {
-            String certPath = "./src/main/resources/DeutscheBankCert.pem";
-            FileInputStream fis = new FileInputStream(certPath);
+            URI certificateUri = new URI(URL_TO_CERTIFICATE);
+            InputStream certs = getCertificates(certificateUri);
 
             // Warning for unchecked assignment can be ignored,
             // as under the hood CertificateFactory#engineGenerateCertificates returns ArrayList<java.security.cert.Certificate>,
             // even though java.util.Collection is mentioned as a return type.
             // See org.bouncycastle.jcajce.provider.asymmetric.x509.CertificateFactory#engineGenerateCertificates implementation for further details.
             @SuppressWarnings("unchecked")
-            Collection<Certificate> certificates = certificateFactory.engineGenerateCertificates(fis);
+            Collection<Certificate> certificates = certificateFactory.engineGenerateCertificates(certs);
 
             if (certificates.isEmpty()) {
                 throw new PsuPasswordEncodingException("No certificates have been provided by bank for PSU password encryption");
@@ -97,6 +96,7 @@ public class DeutscheBankPsuPasswordEncryptionService implements PsuPasswordEncr
                                                            .collect(Collectors.toList());
 
             jweHeader = new JWEHeader.Builder(JWEAlgorithm.RSA_OAEP_256, EncryptionMethod.A256GCM)
+                            .x509CertURL(certificateUri)
                             .x509CertChain(x509CertificateChainEncoded)
                             .build();
 
